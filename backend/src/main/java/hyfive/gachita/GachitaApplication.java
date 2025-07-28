aaf4020dd1529fd67b8a9ee027a97c78ddd7e6d1
jin20fd <syj010820@naver.com>
2025-07-28 16:18:47 +0900

    init: 스프링부트 프로젝트 생성

---
+++ backend/src/main/java/hyfive/gachita/GachitaApplication.java
@@ -0,0 +1,13 @@
+package hyfive.gachita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachitaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/hyfive/gachita/GachitaApplicationTests.java
@@ -0,0 +1,13 @@
+package hyfive.gachita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
