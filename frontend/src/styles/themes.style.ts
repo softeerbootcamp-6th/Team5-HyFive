85d567451abcf3be57d17b5f42132ee753df6931
minjoengss <dotoriido@gmail.com>
2025-07-29 14:30:43 +0900

    style: 폰트, 색상, border radius 토큰 설정

---
+++ frontend/src/styles/themes.style.ts
@@ -0,0 +1,223 @@
+export const theme = {
+  typography: {
+    Heading: {
+      /* Heading/h1_semi */
+      h1_semi: {
+        fontSize: "36px",
+        fontWeight: 600,
+        lineHeight: "44px",
+        letterSpacing: "0%",
+      },
+      /* Heading/h1_medi */
+      h1_medi: {
+        fontSize: "36px",
+        fontWeight: 500,
+        lineHeight: "44px",
+        letterSpacing: "0%",
+      },
+      /* Heading/h2_semi */
+      h2_semi: {
+        fontSize: "32px",
+        fontWeight: 600,
+        lineHeight: "38px",
+        letterSpacing: "0%",
+      },
+      /* Heading/h3_semi */
+      h3_semi: {
+        fontSize: "24px",
+        fontWeight: 600,
+        lineHeight: "28px",
+        letterSpacing: "0%",
+      },
+      /* Heading/h4_semi */
+      h4_semi: {
+        fontSize: "18px",
+        fontWeight: 600,
+        lineHeight: "24px",
+        letterSpacing: "0%",
+      },
+    },
+
+    Body: {
+      /* Body/b1_medi */
+      b1_medi: {
+        fontSize: "24px",
+        fontWeight: 500,
+        lineHeight: "34px",
+        letterSpacing: "0%",
+      },
+      /* Body/b2_semi */
+      b2_semi: {
+        fontSize: "20px",
+        fontWeight: 600,
+        lineHeight: "28px",
+        letterSpacing: "0%",
+      },
+      /* Body/b2_medi */
+      b2_medi: {
+        fontSize: "20px",
+        fontWeight: 500,
+        lineHeight: "28px",
+        letterSpacing: "0%",
+      },
+      /* Body/b3_medi */
+      b3_medi: {
+        fontSize: "18px",
+        fontWeight: 500,
+        lineHeight: "24px",
+        letterSpacing: "0%",
+      },
+      /* Body/b3_regu */
+      b3_regu: {
+        fontSize: "18px",
+        fontWeight: 400,
+        lineHeight: "24px",
+        letterSpacing: "0%",
+      },
+      /* Body/b4_medi */
+      b4_medi: {
+        fontSize: "16px",
+        fontWeight: 500,
+        lineHeight: "22px",
+        letterSpacing: "0%",
+      },
+      /* Body/b4_regu */
+      b4_regu: {
+        fontSize: "16px",
+        fontWeight: 400,
+        lineHeight: "22px",
+        letterSpacing: "0%",
+      },
+    },
+
+    Label: {
+      /* Label/l1_semi */
+      l1_semi: {
+        fontSize: "24px",
+        fontWeight: 600,
+        lineHeight: "28px",
+        letterSpacing: "0%",
+      },
+      /* Label/l2_semi */
+      l2_semi: {
+        fontSize: "20px",
+        fontWeight: 600,
+        lineHeight: "24px",
+        letterSpacing: "0%",
+      },
+      /* Label/l3_semi */
+      l3_semi: {
+        fontSize: "18px",
+        fontWeight: 600,
+        lineHeight: "22px",
+        letterSpacing: "0%",
+      },
+      /* Label/l4_semi */
+      l4_semi: {
+        fontSize: "16px",
+        fontWeight: 600,
+        lineHeight: "20px",
+        letterSpacing: "0%",
+      },
+      /* Label/l5_semi */
+      l5_semi: {
+        fontSize: "14px",
+        fontWeight: 600,
+        lineHeight: "16px",
+        letterSpacing: "0%",
+      },
+    },
+  },
+
+  color: {
+    Maincolor: { primary: " #FF7700" },
+    GrayScale: {
+      white: "#FFFFFF",
+      gray1: "#FAFAFA",
+      gray2: "#DEDEDE",
+      gray3: "#939393",
+      gray4: "#464646",
+      gray5: "#373737",
+      black: "#212121",
+    },
+    Semantic: {
+      success: "#336FFA", // blue 500
+      error: "#F14251", // red 500
+      information: "#18BF81", // green 500
+      warning: "#FFC234", // yellow 400
+    },
+    SemanticScale: {
+      orange: {
+        50: "#FFF8EC",
+        100: "#FFF2D3",
+        200: "#FFE2A5",
+        300: "#FFCC6D",
+        400: "#FFA432",
+        500: "#FF8F0A",
+        600: "#FF7700", // primary
+        700: "#CC5602",
+        800: "#A1430B",
+        900: "#82390C",
+        950: "#461A04",
+      },
+      blue: {
+        50: "#EFF5FF",
+        100: "#DAE7FF",
+        200: "#BED6FF",
+        300: "#91BCFF",
+        400: "#5D98FD",
+        500: "#336FFA", // success
+        600: "#2151EF",
+        700: "#193DDC",
+        800: "#1832B2",
+        900: "#1B32B2",
+        950: "#1C308C",
+      },
+      red: {
+        50: "#FEF2F3",
+        100: "#FEE2E4",
+        200: "#FFCCCE",
+        300: "#FDA4AC",
+        400: "#5D98FD",
+        500: "#F14251", // error
+        600: "#DE4344",
+        700: "#BB1A28",
+        800: "#9A1A25",
+        900: "#801C25",
+        950: "#460609",
+      },
+      green: {
+        50: "#EDFCF4",
+        100: "#D3F8E4",
+        200: "#AAF0CD",
+        300: "#73E2B2",
+        400: "#34CD91",
+        500: "#18BF81", // information
+        600: "#0B9062",
+        700: "#0B7451",
+        800: "#095C42",
+        900: "#094837",
+        950: "#042A1F",
+      },
+      yellow: {
+        50: "#FFFEEB",
+        100: "#FFF3C6",
+        200: "#FFE588",
+        300: "#FFD14A",
+        400: "#FFC234", // warning
+        500: "#F99A07",
+        600: "#DD7302",
+        700: "#B74F06",
+        800: "#943C0C",
+        900: "#7A320C",
+        950: "#461902",
+      },
+    },
+
+    borderRadius: {
+      Small: "8px",
+      Medium: "10px",
+      Large: "12px",
+    },
+  },
+};
