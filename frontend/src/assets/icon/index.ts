7572c0f37302a39e377daf054168948f311d7a1c
minjoengss <dotoriido@gmail.com>
2025-07-29 14:34:23 +0900

    chore: svg icon 폴더 정리 및 일괄 export 설정

---
+++ frontend/src/assets/icon/index.ts
@@ -0,0 +1,49 @@
+import AdminCalender from "./ic_24_admin_calender_default.svg?react";
+import AdminCall from "./ic_24_admin_call_default.svg?react";
+import AdminEmail from "./ic_24_admin_email_default.svg?react";
+import AdminInfo from "./ic_24_admin_info_default.svg?react";
+import AdminPay from "./ic_24_admin_pay_default.svg?react";
+import AdminRefreash from "./ic_24_admin_refreash_default.svg?react";
+import AdminSearch from "./ic_24_admin_search_default.svg?react";
+
+import CenterAdd from "./ic_24_center_add_default.svg?react";
+import CenterCheck from "./ic_24_center_check_default.svg?react";
+import CenterChevronLeft from "./ic_24_center_chevron-left_default.svg?react";
+import CenterChevronRight from "./ic_24_center_chevron-right_default.svg?react";
+import CenterGraph from "./ic_24_center_graph_default.svg?react";
+
+import LendingEye from "./ic_24_lending_eye_default.svg?react";
+import LocationAdmin from "./ic_24_location_admin_default.svg?react";
+
+import NavigationCar from "./ic_24_navigation_car_default.svg?react";
+import NavigationDashboard from "./ic_24_navigation_dashboard_default.svg?react";
+import NavigationPerson from "./ic_24_navigation_person_default.svg?react";
+import NavigationRoute from "./ic_24_navigation_route_default.svg?react";
+
+import AdminCar32 from "./ic_32_admin_car_default.svg?react";
+import CenterAdd32 from "./ic_32_center_add_default.svg?react";
+import CenterCheck32 from "./ic_32_center_check_default.svg?react";
+
+export {
+  AdminCalender,
+  AdminCall,
+  AdminEmail,
+  AdminInfo,
+  AdminPay,
+  AdminRefreash,
+  AdminSearch,
+  CenterAdd,
+  CenterCheck,
+  CenterChevronLeft,
+  CenterChevronRight,
+  CenterGraph,
+  LendingEye,
+  LocationAdmin,
+  NavigationCar,
+  NavigationDashboard,
+  NavigationPerson,
+  NavigationRoute,
+  AdminCar32,
+  CenterAdd32,
+  CenterCheck32,
+};
