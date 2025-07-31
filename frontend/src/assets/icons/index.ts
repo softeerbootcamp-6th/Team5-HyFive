5684d0b887b025ea9ddbaacf22bb9ac1bfb36276
jjamming <ohj3694@naver.com>
2025-07-31 11:18:33 +0900

    style: 변경된 svg 파일 등록 및 컴포넌트화

--- frontend/src/assets/icons/index.ts
+++ frontend/src/assets/icons/index.ts
@@ -1,49 +1,49 @@
-import AdminCalender from "./ic_24_admin_calender_default.svg?react";
-import AdminCall from "./ic_24_admin_call_default.svg?react";
-import AdminEmail from "./ic_24_admin_email_default.svg?react";
-import AdminInfo from "./ic_24_admin_info_default.svg?react";
-import AdminPay from "./ic_24_admin_pay_default.svg?react";
-import AdminRefreash from "./ic_24_admin_refreash_default.svg?react";
-import AdminSearch from "./ic_24_admin_search_default.svg?react";
-
-import CenterAdd from "./ic_24_center_add_default.svg?react";
-import CenterCheck from "./ic_24_center_check_default.svg?react";
-import CenterChevronLeft from "./ic_24_center_chevron-left_default.svg?react";
-import CenterChevronRight from "./ic_24_center_chevron-right_default.svg?react";
-import CenterGraph from "./ic_24_center_graph_default.svg?react";
-
-import LendingEye from "./ic_24_lending_eye_default.svg?react";
-import LocationAdmin from "./ic_24_location_admin_default.svg?react";
-
-import NavigationCar from "./ic_24_navigation_car_default.svg?react";
-import NavigationDashboard from "./ic_24_navigation_dashboard_default.svg?react";
-import NavigationPerson from "./ic_24_navigation_person_default.svg?react";
-import NavigationRoute from "./ic_24_navigation_route_default.svg?react";
-
-import AdminCar32 from "./ic_32_admin_car_default.svg?react";
-import CenterAdd32 from "./ic_32_center_add_default.svg?react";
-import CenterCheck32 from "./ic_32_center_check_default.svg?react";
+import AddIcon from "./add_default.svg?react";
+import CalenderIcon from "./calender_default.svg?react";
+import CallIcon from "./call_default.svg?react";
+import CarIcon from "./car_default.svg?react";
+import CheckCircleIcon from "./check_circle_default.svg?react";
+import ActiveCheckCircleIcon from "./check_circle_active.svg?react";
+import CheckIcon from "./check_default.svg?react";
+import ChevronDownIcon from "./chevron-down_default.svg?react";
+import ChevronLeftIcon from "./chevron-left_default.svg?react";
+import ChevronRightIcon from "./chevron-right_default.svg?react";
+import ChevronUpIcon from "./chevron-up_default.svg?react";
+import DashboardIcon from "./dashboard_default.svg?react";
+import EditIcon from "./edit_default.svg?react";
+import EmailIcon from "./email_default.svg?react";
+import InfoIcon from "./info_default.svg?react";
+import LocationIcon from "./location_default.svg?react";
+import PayIcon from "./pay_default.svg?react";
+import PersonIcon from "./person_default.svg?react";
+import RefreashIcon from "./refreash_default.svg?react";
+import RemoveIcon from "./remove_default.svg?react";
+import RouteIcon from "./route_default.svg?react";
+import SearchIcon from "./search_default.svg?react";
+import TimeIcon from "./time_default.svg?react";
 
 export {
-  AdminCalender,
-  AdminCall,
-  AdminEmail,
-  AdminInfo,
-  AdminPay,
-  AdminRefreash,
-  AdminSearch,
-  CenterAdd,
-  CenterCheck,
-  CenterChevronLeft,
-  CenterChevronRight,
-  CenterGraph,
-  LendingEye,
-  LocationAdmin,
-  NavigationCar,
-  NavigationDashboard,
-  NavigationPerson,
-  NavigationRoute,
-  AdminCar32,
-  CenterAdd32,
-  CenterCheck32,
+  AddIcon,
+  CalenderIcon,
+  CallIcon,
+  CarIcon,
+  CheckCircleIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ChevronUpIcon,
+  DashboardIcon,
+  EditIcon,
+  EmailIcon,
+  InfoIcon,
+  LocationIcon,
+  PayIcon,
+  PersonIcon,
+  RefreashIcon,
+  RemoveIcon,
+  RouteIcon,
+  SearchIcon,
+  TimeIcon,
+  ActiveCheckCircleIcon,
 };
