ce4de28a6bd632f3b9f801fba82f9ff4f13f16c9
minjoengss <dotoriido@gmail.com>
2025-08-02 17:35:29 +0900

    feat: 데이터 타입에 따른 테이블 분리 및 적용

--- frontend/src/utils/TableMatcher.ts
+++ frontend/src/utils/TableMatcher.ts
@@ -1,48 +1,93 @@
-//상수
 const MAPPING_TABLE = {
-  //common
-  name: "이름",
-  phone: "전화번호",
-  isExistWalkingDevice: "보행 기구 유무",
-  bookTime: "예약 접수 시간",
-  bookDate: "이용 희망 날짜",
-  userStartLocation: "출발지",
-  userEndLocation: "목적지",
-  hospitalTime: "병원 도착 시간",
+  //예약자 정보
+  name: { label: "이름", type: "user" },
+  phone: { label: "전화번호", type: "user" },
+  isExistWalkingDevice: { label: "보행 기구 유무", type: "user" },
+  bookTime: { label: "예약 접수 시간", type: "user" },
 
-  //routes
-  routeId: "경로ID",
-  carNumber: "차량번호",
-  routeStartTime: "운행 시작 시간",
-  routeEndTime: "운행 종료 시간",
-  routeStartLocation: "시작점",
-  routeEndLocation: "종점",
+  //예약 정보
+  bookDate: { label: "이용 희망 날짜", type: "booking" },
+  userStartLocation: { label: "출발지", type: "booking" },
+  userEndLocation: { label: "목적지", type: "booking" },
+  hospitalTime: { label: "병원 도착 시간", type: "booking" },
 
-  //orders
-  order: "탑승 순서",
-  boardingTime: "탑승 예정 시간",
-  getOffTime: "하차 예정 시간",
+  //경로 정보
+  routeId: { label: "경로ID", type: "route" },
+  carNumber: { label: "차량번호", type: "route" },
+  routeStartTime: { label: "운행 시작 시간", type: "route" },
+  routeEndTime: { label: "운행 종료 시간", type: "route" },
+  routeStartLocation: { label: "시작점", type: "route" },
+  routeEndLocation: { label: "종점", type: "route" },
 
-  //centers
-  centerName: "센터명",
-  centerLocation: "위치",
-  centerCars: "등록 차량",
-  centerLowCars: "저상형 대수",
+  //탑승 순서 정보
+  order: { label: "탑승 순서", type: "orders" },
+  boardingTime: { label: "탑승 예정 시간", type: "orders" },
+  getOffTime: { label: "하차 예정 시간", type: "orders" },
+
+  //센터 정보
+  centerName: { label: "센터명", type: "centers" },
+  centerLocation: { label: "위치", type: "centers" },
+  centerCars: { label: "등록 차량", type: "centers" },
+  centerLowCars: { label: "저상형 대수", type: "centers" },
 } as const;
 
 //타입
-type TableType = "users" | "books" | "orders" | "centers";
+type TableType = "book" | "orders" | "centers";
+type BookType = "user" | "booking" | "route";
 type TableKey = keyof typeof MAPPING_TABLE;
 export type TableObject = Partial<Record<TableKey, string | number>>;
 
 const TableMatcher = {
   //전체 데이터를 각 테이블 타입에 맞게 분할
-  matchTableType: (data: TableObject[]) => {},
+  matchTableType: (rows: TableObject[]) => {
+    const getDataType = (data: TableObject): TableType => {
+      if ("order" in data) return "orders";
+      if ("centerName" in data) return "centers";
+      return "book";
+    };
+
+    //데이터 타입이 book인 경우 예약자정보, 예약정보, 경로정보로 분할
+    const splitBookData = (rows: TableObject[]) => {
+      const groupData: Record<BookType, TableObject[]> = {
+        user: [],
+        booking: [],
+        route: [],
+      };
+      rows.forEach((row) => {
+        const user: TableObject = {};
+        const booking: TableObject = {};
+        const route: TableObject = {};
+
+        Object.entries(row).forEach(([key, value]) => {
+          const typedKey = key as TableKey;
+          const type = MAPPING_TABLE[typedKey].type;
+          if (type === "user") user[typedKey] = value;
+          else if (type === "booking") booking[typedKey] = value;
+          else if (type === "route") route[typedKey] = value;
+        });
+
+        groupData.user.push(user);
+        groupData.booking.push(booking);
+        groupData.route.push(route);
+      });
+      return groupData;
+    };
+
+    const dataType = getDataType(rows[0]);
+    if (dataType !== "book") return { dataType, originRows: rows };
+    const splitRows = splitBookData(rows);
+    return {
+      dataType,
+      userRows: splitRows.user,
+      bookingRows: splitRows.booking,
+      routeRows: splitRows.route,
+    };
+  },
 
   //전체 행의 key값을 MAPPING_TABEL과 매칭
   matchTableHeader: (rows: TableObject[]) => {
     const keys = Object.keys(rows[0] ?? {}) as TableKey[];
-    const labels = keys.map((key) => MAPPING_TABLE[key]);
+    const labels = keys.map((key) => MAPPING_TABLE[key]["label"]);
     return { keys, labels };
   },
 };
