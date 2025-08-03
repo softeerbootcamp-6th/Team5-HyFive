e78d9da17d1a801ae10c1aa3805093c459e3acca
minjoengss <dotoriido@gmail.com>
2025-08-03 18:01:18 +0900

    feat: 버튼 컴포넌트에 press 애니메이션 할당

--- frontend/src/components/button/Button.tsx
+++ frontend/src/components/button/Button.tsx
@@ -1,5 +1,7 @@
+import usePressDetection from "@/hooks/usePressDetection";
 import { theme } from "@/styles/themes.style";
 import { css } from "@emotion/react";
+import { useState } from "react";
 const { color, typography } = theme;
 
 type Color = "gray" | "orange";
@@ -10,28 +12,44 @@ interface ButtonProps {
 }
 
 const Button = ({ bgColor, size }: ButtonProps) => {
-  return <div css={ButtonContainer(bgColor, size)}>Button</div>;
+  const [isPressing, setIsPressing] = useState(false);
+  const handlers = usePressDetection({
+    setIsPressing,
+  });
+  return (
+    <div {...handlers} css={ButtonContainer(bgColor, size, isPressing)}>
+      Button
+    </div>
+  );
 };
 
 export default Button;
 
-const BackgroundColorMap = {
+const BASE_COLOR_MAP = {
   gray: {
     default: color.GrayScale.gray3,
     hover: color.GrayScale.gray2,
+    press: color.GrayScale.gray3,
   },
   orange: {
     default: color.Maincolor.primary,
     hover: color.SemanticScale.orange[500],
+    press: color.SemanticScale.orange[600],
   },
 };
 
-const ButtonContainer = (bgColor: Color, size: Size) => css`
+const ButtonContainer = (
+  bgColor: Color,
+  size: Size,
+  isPressing: boolean,
+) => css`
   display: flex;
   justify-content: center;
   align-items: center;
   border-radius: 10px;
-  background-color: ${BackgroundColorMap[bgColor].default};
+  background-color: ${isPressing
+    ? BASE_COLOR_MAP[bgColor].press
+    : BASE_COLOR_MAP[bgColor].default};
   width: 100%;
   height: ${size === "small" ? "52px" : "68px"};
   padding: ${size === "small" ? "20px 24px" : "16px 29px"};
@@ -39,8 +57,13 @@ const ButtonContainer = (bgColor: Color, size: Size) => css`
     ? typography.Label.l4_semi
     : typography.Body.b2_semi};
   color: ${color.GrayScale.white};
+  cursor: pointer;
+  transition: all 0.2s ease;
 
-  &:hover {
-    background-color: ${BackgroundColorMap[bgColor].hover};
-  }
+  ${!isPressing &&
+  css`
+    &:hover {
+      background-color: ${BASE_COLOR_MAP[bgColor].hover};
+    }
+  `}
 `;
