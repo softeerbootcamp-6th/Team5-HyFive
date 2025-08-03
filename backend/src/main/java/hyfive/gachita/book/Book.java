6002617b711a462269ecd08fa324c077cfc75797
didyou88 <caujisu2020@gmail.com>
2025-08-03 22:05:36 +0900

    feat : book 패키지 생성 - Book Entity 생성 - Book Repository 생성 - Book Service 생성 - Book Controller 생성 - BookStatus enum 생성

---
+++ backend/src/main/java/hyfive/gachita/book/Book.java
@@ -0,0 +1,86 @@
+package hyfive.gachita.book;
+
+import hyfive.gachita.path.Path;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id", nullable = false, columnDefinition = "BIGINT")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "path_id")
+    private Path path;
+
+    @NotNull
+    @Column(name = "book_name", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String bookName;
+
+    @NotNull
+    @Column(name = "book_tel", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String bookTel;
+
+    @NotNull
+    @Column(name = "book_date", nullable = false, columnDefinition = "DATE")
+    private LocalDate bookDate;
+
+    @NotNull
+    @Column(name = "start_addr", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String startAddr;
+
+    @NotNull
+    @Column(name = "end_addr", nullable = false, columnDefinition = "VARCHAR(255)")
+    private String endAddr;
+
+    @NotNull
+    @Column(name = "walker", nullable = false, columnDefinition = "BOOLEAN")
+    private Boolean walker;
+
+    @NotNull
+    @Column(name = "hospital_book_time", nullable = false, columnDefinition = "TIME")
+    private LocalTime hospitalBookTime;
+
+    @NotNull
+    @Column(name = "deadline", nullable = false, columnDefinition = "TIME")
+    private LocalTime deadline;
+
+    @NotNull
+    @Column(name = "start_lat", nullable = false, columnDefinition = "DECIMAL(9,6)")
+    private double startLat;
+
+    @NotNull
+    @Column(name = "start_lon", nullable = false, columnDefinition = "DECIMAL(9,6)")
+    private double startLon;
+
+    @NotNull
+    @Column(name = "end_lat", nullable = false, columnDefinition = "DECIMAL(9,6)")
+    private double endLat;
+
+    @NotNull
+    @Column(name = "end_lon", nullable = false, columnDefinition = "DECIMAL(9,6)")
+    private double endLon;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "book_status", nullable = false, columnDefinition = "VARCHAR(50)")
+    private BookStatus bookStatus;
+
+    @NotNull
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
+}
+
---
+++ backend/src/main/java/hyfive/gachita/book/BookController.java
@@ -0,0 +1,11 @@
+package hyfive.gachita.book;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/book")
+@RequiredArgsConstructor
+public class BookController {
+}
