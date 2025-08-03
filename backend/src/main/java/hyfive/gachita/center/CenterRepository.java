9bf2885940539ea736309417feefdf9dbef61569
didyou88 <caujisu2020@gmail.com>
2025-08-03 21:44:26 +0900

    chore : CenterRepository 안 쓰는 패키지 제거

--- backend/src/main/java/hyfive/gachita/center/CenterRepository.java
+++ backend/src/main/java/hyfive/gachita/center/CenterRepository.java
@@ -1,6 +1,5 @@
 package hyfive.gachita.center;
 
-import hyfive.gachita.test.Test;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CenterRepository extends JpaRepository<Center, Long> {
