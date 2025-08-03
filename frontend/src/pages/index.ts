53d99d92b27b5af0b650354ce50a5af474272a79
minjoengss <dotoriido@gmail.com>
2025-08-04 00:06:45 +0900

    chore: 원격-로컬 싱크를 맞추기 위한 페이지명 변경이 반영된 index.ts 추가

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -4,12 +4,12 @@ export { default as LandingPage } from "@/pages/LandingPage";
 // Admin
 export { default as DriversPage } from "@/pages/admin/DriversPage";
 export { default as AdminRegisterPage } from "@/pages/admin/AdminRegisterPage";
-export { default as BookPage } from "@/pages/admin/book/BookPage";
-export { default as PathsPage } from "@/pages/admin/book/PathsPage";
-export { default as SchedulePage } from "@/pages/admin/book/SchedulePage";
-export { default as UsersPage } from "@/pages/admin/book/UsersPage";
-export { default as CentersPage } from "@/pages/admin/centers/CentersPage";
-export { default as CenterDetailPage } from "@/pages/admin/centers/CenterDetailPage";
+export { default as BookPage } from "@/pages/admin/book/BookPage_test";
+export { default as PathsPage } from "@/pages/admin/book/PathsPage_test";
+export { default as SchedulePage } from "@/pages/admin/book/SchedulePage_test";
+export { default as UsersPage } from "@/pages/admin/book/UsersPage_test";
+export { default as CentersPage } from "@/pages/admin/centers/CentersPage_test";
+export { default as CenterDetailPage } from "@/pages/admin/centers/CenterDetailPage_test";
 
 // Center
 export { default as CenterPage } from "@/pages/center/CenterPage";
