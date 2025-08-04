04ad24e7ce0b0e97b16ed57aefb6f98f7721871c
minjoengss <dotoriido@gmail.com>
2025-08-05 01:01:43 +0900

    feat: 개별 예약 정보 조회 영역 구현

---
+++ frontend/src/features/book/BookDetailSection.tsx
@@ -0,0 +1,62 @@
+import Table from "@/components/table/Table";
+import type { BookType } from "@/features/book/Book.types";
+import RouteCard from "@/features/book/RouteCard";
+import { theme } from "@/styles/themes.style";
+import type { TableObject } from "@/utils/TableMatcher";
+import { css } from "@emotion/react";
+const { typography } = theme;
+
+interface BookDetailSectionProps {
+  activeTab: BookType;
+  userRows: TableObject[];
+  bookingRows: TableObject[];
+  routeRows: TableObject[];
+}
+
+const BookDetailSection = ({
+  activeTab,
+  userRows,
+  bookingRows,
+  routeRows,
+}: BookDetailSectionProps) => {
+  const TABLE_SECTIONS = [
+    { title: "예약자 정보", data: userRows },
+    { title: "예약 정보", data: bookingRows },
+    { title: "이용 경로 정보", data: routeRows },
+  ];
+
+  return (
+    <div css={BookDetailSectionContainer}>
+      {TABLE_SECTIONS.map(({ title, data }) => (
+        <div css={TableWrapper} key={title}>
+          <p css={TitleText}>{title}</p>
+          {title === "이용 경로 정보" && activeTab !== "success" ? (
+            <RouteCard routeType={activeTab} />
+          ) : (
+            <Table rows={data} />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BookDetailSection;
+
+const BookDetailSectionContainer = css`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  padding: 40px;
+`;
+
+const TableWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+const TitleText = css`
+  font: ${typography.Heading.h5_semi};
+`;
