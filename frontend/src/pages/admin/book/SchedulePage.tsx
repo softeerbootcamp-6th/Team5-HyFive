c78d1cd4c75a6140ee21782afd6ae63df390ad02
minjoengss <dotoriido@gmail.com>
2025-08-06 00:53:09 +0900

    feat: 전체 운행 정보 컴포넌트 구현

--- frontend/src/pages/admin/book/SchedulePage.tsx
+++ frontend/src/pages/admin/book/SchedulePage.tsx
@@ -1,5 +1,25 @@
+import type { ScheduleType } from "@/features/schedule/Schedule.types";
+import ScheduleListSection from "@/features/schedule/ScheduleListSection";
+import { css } from "@emotion/react";
+import { useState } from "react";
+
 const SchedulePage = () => {
-  return <>Schedule</>;
+  const TAB_LIST: ScheduleType[] = ["waiting", "inProgress", "completed"];
+  const [activeTab, setActiveTab] = useState<ScheduleType>(TAB_LIST[0]);
+  return (
+    <div css={BookPageContainer}>
+      <ScheduleListSection
+        TAB_LIST={TAB_LIST}
+        activeTab={activeTab}
+        setActiveTab={setActiveTab}
+      />
+    </div>
+  );
 };
 
 export default SchedulePage;
+
+const BookPageContainer = css`
+  display: flex;
+  flex-direction: row;
+`;
