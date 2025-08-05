10293033121a4244fd680b63d463be4c656b8278
minjoengss <dotoriido@gmail.com>
2025-08-05 18:25:43 +0900

    refactor: TabMatcher 내부 if문 매칭을 매핑 테이블로 변경

--- frontend/src/utils/TabMatcher.ts
+++ frontend/src/utils/TabMatcher.ts
@@ -1,11 +1,13 @@
 import type { BookType } from "@/features/book/Book.types";
 
+const MAPPING_TABLE = {
+  pending: "신규 예약",
+  success: "예약 성공",
+  fail: "예약 실패",
+};
 const TabMatcher = {
   matchInnerTypeToExternalType: (activeTab: BookType): string => {
-    if (activeTab === "pending") return "신규 예약";
-    if (activeTab === "success") return "예약 성공";
-    if (activeTab === "fail") return "예약 실패";
-    return "";
+    return MAPPING_TABLE[activeTab];
   },
 };
 
