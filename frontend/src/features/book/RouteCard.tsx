2e85a0f29bbf8ba07d963fe9422a6c898021d205
minjoengss <dotoriido@gmail.com>
2025-08-05 14:24:46 +0900

    fix: 예약 대기-취소 종류에 따라 이미지 제시 방법 분리

--- frontend/src/features/book/RouteCard.tsx
+++ frontend/src/features/book/RouteCard.tsx
@@ -9,7 +9,7 @@ const RouteCard = ({ routeType }: { routeType: RouteType }) => {
   const ROUTE_LIST = {
     pending: {
       label: "보행 기구 유무에 맞는 차량을 찾고 있어요",
-      img: <img src={MovingCarGif} />,
+      img: <img src={MovingCarGif} alt="moving car gif" />,
     },
     fail: {
       label: "이용 가능한 경로가 없어요",
@@ -19,7 +19,7 @@ const RouteCard = ({ routeType }: { routeType: RouteType }) => {
   return (
     <div css={RouteCardContainer}>
       <p css={RouteResultText}>{ROUTE_LIST[routeType].label}</p>
-      <div css={RouteResultImg}>{ROUTE_LIST[routeType].img}</div>
+      <div css={RouteResultImg(routeType)}>{ROUTE_LIST[routeType].img}</div>
     </div>
   );
 };
@@ -43,9 +43,15 @@ const RouteResultText = css`
   z-index: 1;
 `;
 
-const RouteResultImg = css`
-  position: absolute;
-  top: 0;
-  left: 0;
+const RouteResultImg = (routeType: RouteType) => css`
+  ${routeType === "pending" &&
+  css`
+    position: absolute;
+    top: 0;
+    left: 0;
+  `}
   width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
