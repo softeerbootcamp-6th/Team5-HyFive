17888621bc84d44afbc9aebb581ce4fb0ea65d0b
jin20fd <syj010820@naver.com>
2025-08-05 15:30:52 +0900

    feat: 동일한 사용자의 중복 예약 예외 처리 추가

--- backend/src/main/java/hyfive/gachita/book/BookRepository.java
+++ backend/src/main/java/hyfive/gachita/book/BookRepository.java
@@ -2,5 +2,9 @@ package hyfive.gachita.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    boolean existsBookByBookTelAndHospitalDate(String bookTel, LocalDate hospitalDate);
 }
--- backend/src/main/java/hyfive/gachita/book/BookService.java
+++ backend/src/main/java/hyfive/gachita/book/BookService.java
@@ -1,6 +1,8 @@
 package hyfive.gachita.book;
 
 import hyfive.gachita.book.dto.CreateBookReq;
+import hyfive.gachita.common.response.BusinessException;
+import hyfive.gachita.common.response.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,10 @@ public class BookService {
     private final BookRepository bookRepository;
 
     public Book createBook(CreateBookReq createBookReq) {
+        if (bookRepository.existsBookByBookTelAndHospitalDate(createBookReq.getBookTel(), createBookReq.getHospitalDate())) {
+            throw new BusinessException(ErrorCode.INVALID_INPUT, "예약 불가: 동일한 사용자에 의해 이미 예약된 날짜입니다.");
+        }
+
         //TODO 출발지, 도착지의 위도, 경도 설정
         Book book = Book.builder()
                 .bookName(createBookReq.getBookName())
