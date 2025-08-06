b2d0e2b4fd7516369eae9b216f190136bd5e2d10
didyou88 <caujisu2020@gmail.com>
2025-08-06 16:14:27 +0900

    chore : CreateCarReq imgFile 필드 Schema description 수정

--- backend/src/main/java/hyfive/gachita/car/dto/CreateCarReq.java
+++ backend/src/main/java/hyfive/gachita/car/dto/CreateCarReq.java
@@ -30,7 +30,7 @@ public record CreateCarReq(
         Boolean lowFloor,
 
         @NotNull
-        @Schema(description = "차량 이미지 URL", example = "이미지는 최대 10MB까지 첨부 가능합니다")
+        @Schema(description = "차량 이미지 파일", example = "이미지는 최대 10MB까지 첨부 가능합니다")
         MultipartFile imgFile
 ) {}
 
