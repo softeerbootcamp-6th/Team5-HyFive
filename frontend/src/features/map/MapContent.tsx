12ffc6bdc84bd4af6938dca36c6653d46a9f37d7
minjoengss <dotoriido@gmail.com>
2025-08-06 21:54:32 +0900

    feat: ref와 지도 연결하여 화면에 제시

--- frontend/src/features/map/MapContent.tsx
+++ frontend/src/features/map/MapContent.tsx
@@ -1,9 +1,27 @@
 import { css } from "@emotion/react";
 import { useEffect, useRef } from "react";
 
+declare global {
+  interface Window {
+    kakao: any;
+  }
+}
+
 const MapContent = () => {
   const mapRef = useRef(null);
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const container = mapRef.current;
+    if (!container) {
+      return;
+    }
+
+    const options = {
+      center: new window.kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const initializedMap = new window.kakao.maps.Map(container, options);
+  }, []);
   return <div id="map" ref={mapRef} css={MapContentContainer} />;
 };
 
---
+++ frontend/src/features/map/useInitMap.ts
@@ -0,0 +1,3 @@
+const useInitMap = () => {};
+
+export default useInitMap;
