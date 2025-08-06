ffb863c29d1e526ad4b7984bdace7d63a0cdad94
didyou88 <caujisu2020@gmail.com>
2025-08-06 20:07:25 +0900

    chore : CarRes Swagger Schema 추가

--- backend/src/main/java/hyfive/gachita/car/dto/CarRes.java
+++ backend/src/main/java/hyfive/gachita/car/dto/CarRes.java
@@ -1,17 +1,34 @@
 package hyfive.gachita.car.dto;
 
 import hyfive.gachita.car.Car;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CarRes(
-         Long id,
-         Long centerId,
-         String centerName,
-         String modelName,
-         String carNumber,
-         int capacity,
-         Boolean lowFloor,
-         String carImage
-) {
+        @Schema(description = "차량 ID", example = "1")
+        Long id,
+
+        @Schema(description = "차량이 소속된 센터 ID", example = "1")
+        Long centerId,
+
+        @Schema(description = "센터명", example = "공릉노인복지관")
+        String centerName,
+
+        @Schema(description = "차량 모델명", example = "기아 레이")
+        String modelName,
+
+        @Schema(description = "차량 번호", example = "12가 3456")
+        String carNumber,
+
+        @Schema(description = "탑승 가능 인원 수", example = "3")
+        int capacity,
+
+        @Schema(description = "저상 차량 여부", example = "false")
+        Boolean lowFloor,
+
+        // TODO : carImage example 추가
+        @Schema(description = "차량 이미지 url", example = "(차후 추가 예정)")
+        String carImage
+    ) {
     public static CarRes from(Car car) {
         return new CarRes(
                 car.getId(),
