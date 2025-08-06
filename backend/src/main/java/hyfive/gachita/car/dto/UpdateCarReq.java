0f31c5308fc45b62ac09e1e2b43d9a48129aa637
didyou88 <caujisu2020@gmail.com>
2025-08-06 20:54:44 +0900

    chore : UpdateCarReq imgFile schema description 수정

--- backend/src/main/java/hyfive/gachita/car/dto/UpdateCarReq.java
+++ backend/src/main/java/hyfive/gachita/car/dto/UpdateCarReq.java
@@ -25,7 +25,7 @@ public record UpdateCarReq(
         Boolean lowFloor,
 
         @NotNull
-        @Schema(description = "차량 이미지 URL", example = "이미지는 최대 10MB까지 첨부 가능합니다")
+        @Schema(description = "차량 이미지 파일", example = "이미지는 최대 10MB까지 첨부 가능합니다")
         MultipartFile imgFile
 ) {}
 
