5dfaf72562862dc9b45858d75b477db214efa343
didyou88 <caujisu2020@gmail.com>
2025-08-06 15:07:24 +0900

    feat : CarDocs 추가

--- backend/src/main/java/hyfive/gachita/car/CarController.java
+++ backend/src/main/java/hyfive/gachita/car/CarController.java
@@ -1,7 +1,9 @@
 package hyfive.gachita.car;
+
 import hyfive.gachita.car.dto.CarRes;
 import hyfive.gachita.car.dto.CreateCarReq;
 import hyfive.gachita.common.response.BaseResponse;
+import hyfive.gachita.docs.CarDocs;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
@@ -10,11 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/car")
 @RequiredArgsConstructor
-public class CarController {
+public class CarController implements CarDocs {
     private final CarService carService;
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public BaseResponse<CarRes> createBook(@ModelAttribute @Validated CreateCarReq createCarReq) {
+    public BaseResponse<CarRes> createCar(@ModelAttribute @Validated CreateCarReq createCarReq) {
         Car createCar = carService.createCar(createCarReq);
         return BaseResponse.success(CarRes.from(createCar));
     }
