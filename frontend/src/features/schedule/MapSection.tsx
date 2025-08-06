0bd69195fada7c9c8c7c68dc77c6a48e4ca9af4d
minjoengss <dotoriido@gmail.com>
2025-08-06 14:13:18 +0900

    feat: 개별 운행 경로 내부 지도 영역 할당

--- frontend/src/features/schedule/MapSection.tsx
+++ frontend/src/features/schedule/MapSection.tsx
@@ -1,3 +1,4 @@
+import MapContent from "@/features/schedule/MapContent";
 import MapHeader from "@/features/schedule/MapHeader";
 import type { ScheduleType } from "@/features/schedule/Schedule.types";
 import { css } from "@emotion/react";
@@ -14,6 +15,7 @@ const MapSection = ({ scheduleType, data }: MapSectionProps) => {
   return (
     <div css={MapSectionContainer}>
       <MapHeader scheduleType={scheduleType} data={data} />
+      <MapContent />
     </div>
   );
 };
@@ -23,5 +25,8 @@ export default MapSection;
 const MapSectionContainer = css`
   display: flex;
   flex-direction: column;
+  gap: 40px;
+  padding: 20px 40px 60px 42px;
   width: 100%;
+  height: 100%;
 `;
