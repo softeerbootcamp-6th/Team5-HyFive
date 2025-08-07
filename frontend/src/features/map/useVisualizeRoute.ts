f644b984851dd72a411a71f38ca8b76a6705aa97
minjoengss <dotoriido@gmail.com>
2025-08-07 17:39:33 +0900

    feat: Polyline 표시 로직 커스텀 훅으로 분리

--- frontend/src/features/map/useVisualizeRoute.ts
+++ frontend/src/features/map/useVisualizeRoute.ts
@@ -1,3 +1,28 @@
-const useVisualizeRoute = () => {};
+import type { Path } from "@/types/path.types";
+import { useEffect } from "react";
+
+interface UseVisualizeRouteProps {
+  map: any;
+  path: Path[];
+}
+const useVisualizeRoute = ({ map, path }: UseVisualizeRouteProps) => {
+  useEffect(() => {
+    if (!map) return;
+
+    const linePath = path.map(
+      (point) => new window.kakao.maps.LatLng(point.lng, point.lat),
+    );
+
+    const polyline = new window.kakao.maps.Polyline({
+      path: linePath,
+      strokeWeight: 16,
+      strokeColor: "#F70",
+      strokeOpacity: 1,
+      strokeStyle: "solid",
+    });
+
+    polyline.setMap(map);
+  }, [map]);
+};
 
 export default useVisualizeRoute;
