aba4a273bc8f6c0ee1173114a0821a27bdd3f8d3
minjoengss <dotoriido@gmail.com>
2025-08-10 17:17:09 +0900

    refactor: 단일 segment 반환하는 기능을 복수 segment 반환하는 기능으로 확장

--- frontend/src/features/map/getRouteMidPoint.test.ts
+++ frontend/src/features/map/getRouteMidPoint.test.ts
@@ -1,6 +1,6 @@
 import getRouteMidPoint from "@/features/map/getRouteMidPoint.util";
 
-const path = [
+const TEST_PATH = [
   { lat: 37.65350753605161, lng: 127.05272873217041 },
   { lat: 37.65325505753623, lng: 127.05318184012343 },
   { lat: 37.65311067453145, lng: 127.05368035131548 },
@@ -11,7 +11,7 @@ const path = [
 describe("지도의 중점을 올바르게 반환한다.", () => {
   it("전체 경로의 길이가 홀수인 경우 중앙값에 해당하는 위치를 반환한다.", () => {
     //when
-    const centerLatLng = getRouteMidPoint(path);
+    const centerLatLng = getRouteMidPoint(TEST_PATH);
 
     //then
     expect(centerLatLng).toEqual({
@@ -22,7 +22,9 @@ describe("지도의 중점을 올바르게 반환한다.", () => {
 
   it("전체 경로의 길이가 짝수인 경우 중앙 기준 좌측에 해당하는 위치를 반환한다.", () => {
     //when
-    const centerLatLng = getRouteMidPoint(path.slice(0, path.length - 1));
+    const centerLatLng = getRouteMidPoint(
+      TEST_PATH.slice(0, TEST_PATH.length - 1),
+    );
 
     //then
     expect(centerLatLng).toEqual({
@@ -33,7 +35,7 @@ describe("지도의 중점을 올바르게 반환한다.", () => {
 
   it("전체 경로의 길이가 1인 경우 해당 요소의 위치를 반환한다.", () => {
     //when
-    const centerLatLng = getRouteMidPoint(path.slice(0, 1));
+    const centerLatLng = getRouteMidPoint(TEST_PATH.slice(0, 1));
 
     //then
     expect(centerLatLng).toEqual({
@@ -44,7 +46,7 @@ describe("지도의 중점을 올바르게 반환한다.", () => {
 
   it("전체 경로의 길이가 음수거나 0인 경우 null인 위치를 반환한다.", () => {
     //when
-    const centerLatLng = getRouteMidPoint(path.slice(0, 0));
+    const centerLatLng = getRouteMidPoint(TEST_PATH.slice(0, 0));
 
     //then
     expect(centerLatLng).toEqual({ lat: undefined, lng: undefined });
