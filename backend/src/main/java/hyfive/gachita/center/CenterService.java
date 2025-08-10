adc08180b30c4eed9152c27610bf5d003f0dc7fc
jin20fd <syj010820@naver.com>
2025-08-10 15:00:29 +0900

    feat: 등록 차량 대수 추가

--- backend/src/main/java/hyfive/gachita/center/CenterService.java
+++ backend/src/main/java/hyfive/gachita/center/CenterService.java
@@ -11,8 +11,14 @@ import org.springframework.stereotype.Service;
 public class CenterService {
     private final CenterRepository centerRepository;
 
-    public Center getCenter(Long id) {
-        return centerRepository.findById(id)
+    public CenterRes getCenter(Long id) {
+        Center center = centerRepository.findById(id)
                 .orElseThrow(()-> new BusinessException(ErrorCode.NO_EXIST_VALUE, "DB에 센터 데이터가 존재하지 않습니다."));
+        return CenterRes.builder()
+                .centerName(center.getCenterName())
+                .centerAddr(center.getCenterAddr())
+                .centerTel(center.getCenterTel())
+                .carCount(center.getCarList().size())
+                .build();
     }
 }
