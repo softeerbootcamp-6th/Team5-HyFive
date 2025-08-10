a95b235174553dfae590946c346cfe217a97527a
jin20fd <syj010820@naver.com>
2025-08-10 15:29:31 +0900

    feat: 일주일 정산 금액 추가

--- backend/src/main/java/hyfive/gachita/center/CenterService.java
+++ backend/src/main/java/hyfive/gachita/center/CenterService.java
@@ -1,8 +1,8 @@
 package hyfive.gachita.center;
 
-import hyfive.gachita.car.Car;
 import hyfive.gachita.common.response.BusinessException;
 import hyfive.gachita.common.response.ErrorCode;
+import hyfive.gachita.pay.PayService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,15 +10,20 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CenterService {
     private final CenterRepository centerRepository;
+    private final PayService payService;
 
     public CenterRes getCenter(Long id) {
         Center center = centerRepository.findById(id)
                 .orElseThrow(()-> new BusinessException(ErrorCode.NO_EXIST_VALUE, "DB에 센터 데이터가 존재하지 않습니다."));
+
+        int weeklyPayAmount = payService.getWeeklyPayAmount(center.getId());
+
         return CenterRes.builder()
                 .centerName(center.getCenterName())
                 .centerAddr(center.getCenterAddr())
                 .centerTel(center.getCenterTel())
                 .carCount(center.getCarList().size())
+                .payAmount(weeklyPayAmount)
                 .build();
     }
 }
