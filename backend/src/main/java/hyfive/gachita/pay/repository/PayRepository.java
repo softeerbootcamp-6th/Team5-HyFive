7c7010b72d76c3c67f4de220b1101c5fa929aff7
jin20fd <syj010820@naver.com>
2025-08-11 18:21:07 +0900

    chore: 사용하지 않는 인터페이스 함수 정의 제거

--- backend/src/main/java/hyfive/gachita/pay/repository/PayRepository.java
+++ backend/src/main/java/hyfive/gachita/pay/repository/PayRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.util.Pair;
 import java.time.LocalDateTime;
 
 public interface PayRepository extends JpaRepository<Pay, Long>, CustomPayRepository {
-    int getAmountByPeriod(Long centerId, Pair<LocalDateTime, LocalDateTime> period);
 }
