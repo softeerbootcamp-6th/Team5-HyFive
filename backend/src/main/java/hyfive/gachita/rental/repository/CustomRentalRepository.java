df081cfd2523212d035ac4be82a541e828d44734
didyou88 <caujisu2020@gmail.com>
2025-08-11 16:40:17 +0900

    feat : CustomRentalRepository 유휴 시간 리스트 로직 추가

--- backend/src/main/java/hyfive/gachita/rental/repository/CustomRentalRepository.java
+++ backend/src/main/java/hyfive/gachita/rental/repository/CustomRentalRepository.java
@@ -1,8 +1,13 @@
 package hyfive.gachita.rental.repository;
 
+import hyfive.gachita.rental.dto.RentalRes;
+
 import java.time.LocalDate;
+import java.util.List;
 
 public interface CustomRentalRepository {
 
     void deleteRentalsBetween(Long carId, LocalDate startDate, LocalDate endDate);
+
+    List<RentalRes> findRentalsBetween(Long carId, LocalDate startDate, LocalDate endDate);
 }
--- backend/src/main/java/hyfive/gachita/rental/repository/CustomRentalRepositoryImpl.java
+++ backend/src/main/java/hyfive/gachita/rental/repository/CustomRentalRepositoryImpl.java
@@ -1,11 +1,16 @@
 package hyfive.gachita.rental.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import hyfive.gachita.car.dto.CarListRes;
+import hyfive.gachita.rental.Rental;
+import hyfive.gachita.rental.dto.RentalRes;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import static hyfive.gachita.car.QCar.car;
 import static hyfive.gachita.rental.QRental.rental;
 
 @Repository
@@ -23,5 +28,20 @@ public class CustomRentalRepositoryImpl implements CustomRentalRepository {
                                 .and(rental.rentalDate.between(startDate, endDate))
                 )
                 .execute();
+    }
+
+    @Override
+    public List<RentalRes> findRentalsBetween(Long carId, LocalDate startDate, LocalDate endDate) {
+        List<Rental> rentalList = queryFactory
+                .selectFrom(rental)
+                .where(
+                        rental.car.id.eq(carId)
+                                .and(rental.rentalDate.between(startDate, endDate))
+                )
+                .fetch();
+
+        return rentalList.stream()
+                .map(RentalRes::from)
+                .toList();
     }
 }
