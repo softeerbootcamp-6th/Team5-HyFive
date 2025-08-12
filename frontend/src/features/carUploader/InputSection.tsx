27e2b3ef87ca382c95c0f3c5779fd208dfc31af2
minjoengss <dotoriido@gmail.com>
2025-08-13 00:27:37 +0900

    fix: isLowFloor 선택시 전체 유효성 검사 진행하는 문제 해결하기 위해 ToggleButton type에 button 할당

--- frontend/src/features/carUploader/InputSection.tsx
+++ frontend/src/features/carUploader/InputSection.tsx
@@ -14,20 +14,18 @@ const InputSection = () => {
   const dropdownOptions = Array.from({ length: MAX_PASSENGER }, (_, index) =>
     String(index + 1),
   );
-  const carSchema = z
-    .object({
-      carModel: z.string().min(1, "필수 입력값입니다"),
-      carNumber: z
-        .string()
-        .refine((value) => /^\d{2,3}[가-힣]\d{4}$/.test(value), {
-          message: "올바른 차량 번호 형식이 아닙니다 (예: 12가3456)",
-        }),
-      maxPassenger: z.string().refine((value) => value !== "", {
-        message: "필수 입력값입니다",
+  const carSchema = z.object({
+    carModel: z.string().min(1, "필수 입력값입니다"),
+    carNumber: z
+      .string()
+      .refine((value) => /^\d{2,3}[가-힣]\d{4}$/.test(value), {
+        message: "올바른 차량 번호 형식이 아닙니다 (예: 12가3456)",
       }),
-      isLowFloor: z.boolean(),
-    })
-    .refine((data) => data.carModel);
+    maxPassenger: z.string().refine((value) => value !== "", {
+      message: "필수 입력값입니다",
+    }),
+    isLowFloor: z.boolean(),
+  });
 
   const {
     register,
