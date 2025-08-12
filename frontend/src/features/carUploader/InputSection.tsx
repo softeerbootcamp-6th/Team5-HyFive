16c14526a7eb1899a36a7c16bebd0a8b72ee4ce0
minjoengss <dotoriido@gmail.com>
2025-08-12 22:04:14 +0900

    feat: react-hook-form register, control 적용해 입력값 감시

--- frontend/src/features/carUploader/InputSection.tsx
+++ frontend/src/features/carUploader/InputSection.tsx
@@ -4,43 +4,72 @@ import ToggleButton from "@/components/ToggleButton";
 import ImageInput from "@/features/carUploader/ImageInput";
 import { css } from "@emotion/react";
 import OriginCarImg from "@/assets/images/OriginalCarImg.png";
+import Button from "@/components/Button";
+import { Controller, useForm } from "react-hook-form";
 
 const InputSection = () => {
-  const dropdownOptions = Array.from({ length: 5 }, (_, index) =>
-    String(index + 1),
-  );
+  const dropdownOptions = Array.from({ length: 5 }, (_, index) => index + 1);
+
+  const { register, control, handleSubmit } = useForm({
+    defaultValues: {
+      carModel: "",
+      carNumber: "",
+      maxPassenger: 0,
+      isLowFloor: false,
+    },
+  });
 
   return (
-    <div css={InputSectionContainer}>
+    <form
+      css={InputSectionContainer}
+      onSubmit={handleSubmit((e) => console.log(e))}
+    >
       <ImageInput imageSrc={OriginCarImg} />
       <Input
         label={"모델명"}
         required={true}
         placeholder="모델명을 입력해주세요"
         readOnly={false}
+        register={register("carModel", { required: true })}
       />
       <Input
         label={"차량 번호"}
         required={true}
         placeholder="차량 번호를 입력해주세요"
         readOnly={false}
+        register={register("carNumber", { required: true })}
       />
-      <DropdownInput
-        label="최대탑승 인원"
-        required={true}
-        requiredLabel="운전자 제외"
-        placeholder="인원"
-        options={dropdownOptions}
-        value={""}
-        onSelect={() => {}}
+      <Controller
+        name="maxPassenger"
+        control={control}
+        render={({ field }) => (
+          <DropdownInput
+            label="최대탑승 인원"
+            required={true}
+            requiredLabel="운전자 제외"
+            placeholder="인원"
+            options={dropdownOptions}
+            value={field.value}
+            onSelect={(value) => field.onChange(value)}
+          />
+        )}
       />
-      <ToggleButton
-        type="check"
-        label="저상형"
-        isChecked={true}
-        onToggle={() => {}}
+      <Controller
+        name="isLowFloor"
+        control={control}
+        render={({ field }) => (
+          <ToggleButton
+            type="check"
+            label="저상형"
+            isChecked={field.value}
+            onToggle={() => {
+              field.onChange(!field.value);
+            }}
+          />
+        )}
       />
-    </div>
+      <Button type="submit" bgColor="gray" label="등록하기" size="big" />
+    </form>
   );
 };
 
