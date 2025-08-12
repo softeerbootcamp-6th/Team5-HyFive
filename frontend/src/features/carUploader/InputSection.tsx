d62b3546e695c52d2842e27731405110ca52e1ca
minjoengss <dotoriido@gmail.com>
2025-08-12 15:14:16 +0900

    feat: 사진 등록 컴포넌트 및 공통 컴포넌트의 인풋 컴포넌트 페이지에 등록

---
+++ frontend/src/features/carUploader/InputSection.tsx
@@ -0,0 +1,51 @@
+import DropdownInput from "@/components/DropdownInput";
+import Input from "@/components/Input";
+import ToggleButton from "@/components/ToggleButton";
+import ImageInput from "@/features/carUploader/ImageInput";
+import { css } from "@emotion/react";
+
+const InputSection = () => {
+  const dropdownOptions = Array.from({ length: 5 }, (_, index) =>
+    String(index + 1),
+  );
+  return (
+    <div css={InputSectionContainer}>
+      <ImageInput />
+      <Input
+        label={"모델명"}
+        required={true}
+        placeholder="모델명을 입력해주세요"
+        readOnly={false}
+      />
+      <Input
+        label={"차량 번호"}
+        required={true}
+        placeholder="차량 번호를 입력해주세요"
+        readOnly={false}
+      />
+      <DropdownInput
+        label="최대탑승 인원"
+        required={true}
+        requiredLabel="운전자 제외"
+        placeholder="인원"
+        options={dropdownOptions}
+        value={""}
+        onSelect={() => {}}
+      />
+      <ToggleButton
+        type="check"
+        label="저상형"
+        isChecked={true}
+        onToggle={() => {}}
+      />
+    </div>
+  );
+};
+
+export default InputSection;
+
+const InputSectionContainer = css`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
--- frontend/src/pages/center/CenterRegisterPage.tsx
+++ frontend/src/pages/center/CenterRegisterPage.tsx
@@ -1,13 +1,30 @@
-import ImageUploader from "@/features/imageUploader/ImageUploader";
+import { css } from "@emotion/react";
+import { theme } from "@/styles/themes.style";
+import Button from "@/components/Button";
+import InputSection from "@/features/carUploader/InputSection";
+
+const { color, typography } = theme;
 
 const CenterRegisterPage = () => {
   return (
-    <div>
-      <p>차량 등록하기</p>
-      <div></div>
-      <ImageUploader />
+    <div css={CenterRegisterPageContainer}>
+      <p css={HeaderText}>차량 등록하기</p>
+      <InputSection />
+      <Button bgColor="gray" label="등록하기" size="big" />
     </div>
   );
 };
 
 export default CenterRegisterPage;
+
+const CenterRegisterPageContainer = css`
+  display: flex;
+  flex-direction: column;
+  gap: 44px;
+  max-width: 660px;
+  margin: 44px auto;
+`;
+
+const HeaderText = css`
+  font: ${typography.Heading.h3_semi};
+`;
