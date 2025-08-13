64c2de31be31a2340fb5932ae27a5b3e30937b75
minjoengss <dotoriido@gmail.com>
2025-08-13 11:40:02 +0900

    refactor: 재생성 방지 위해 schema 생성 로직 커스텀 훅 외부로 이동

--- frontend/src/features/carUploader/useCarForm.ts
+++ frontend/src/features/carUploader/useCarForm.ts
@@ -2,20 +2,18 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
-const useCarForm = () => {
-  const carSchema = z.object({
-    carModel: z.string().min(1, "필수 입력값입니다"),
-    carNumber: z
-      .string()
-      .refine((value) => /^\d{2,3}[가-힣]\d{4}$/.test(value), {
-        message: "올바른 차량 번호 형식이 아닙니다 (예: 12가3456)",
-      }),
-    maxPassenger: z.string().refine((value) => value !== "", {
-      message: "필수 입력값입니다",
-    }),
-    isLowFloor: z.boolean(),
-  });
+const carSchema = z.object({
+  carModel: z.string().min(1, "필수 입력값입니다"),
+  carNumber: z.string().refine((value) => /^\d{2,3}[가-힣]\d{4}$/.test(value), {
+    message: "올바른 차량 번호 형식이 아닙니다 (예: 12가3456)",
+  }),
+  maxPassenger: z.string().refine((value) => value !== "", {
+    message: "필수 입력값입니다",
+  }),
+  isLowFloor: z.boolean(),
+});
 
+const useCarForm = () => {
   const { register, control, handleSubmit, reset, formState } = useForm({
     mode: "onSubmit",
     resolver: zodResolver(carSchema),
