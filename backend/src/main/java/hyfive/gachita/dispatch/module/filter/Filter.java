c4f05770fd3893bf8f6f0ef22e2eceab8cc249a4
didyou88 <caujisu2020@gmail.com>
2025-08-13 18:17:27 +0900

    chore : 인터페이스 사용법 주석 추가

--- backend/src/main/java/hyfive/gachita/dispatch/module/filter/Filter.java
+++ backend/src/main/java/hyfive/gachita/dispatch/module/filter/Filter.java
@@ -5,6 +5,13 @@ import hyfive.gachita.dispatch.module.filter.condition.FilterCondition;
 
 import java.util.List;
 
+/**
+ * 요약 : 필터링 인터페이스
+ * @param : 필터링 전 후보들(candidates), 필터링 조건(condition)
+ * @return : 필터링 결과 리스트 List<DispatchLocation>
+ * 사용 방법 :
+ *  - (예시 구현체) HarversineFilter.java
+ */
 public interface Filter<C extends FilterCondition> {
     List<DispatchLocation> filter(List<DispatchLocation> candidates, C condition);
 }
