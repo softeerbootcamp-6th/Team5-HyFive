4b2c58a1400931a3e713e419b43a49dc7afb3f90
jin20fd <syj010820@naver.com>
2025-08-13 23:25:09 +0900

    feat: PathSegment, Point Entity 생성

---
+++ backend/src/main/java/hyfive/gachita/path/PathSegment.java
@@ -0,0 +1,29 @@
+package hyfive.gachita.path;
+
+import hyfive.gachita.node.Node;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "path_segment")
+public class PathSegment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "start_id")
+    private Node startNode;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "end_id")
+    private Node endNode;
+
+    @Column(name = "duration", nullable = false, columnDefinition = "INT")
+    private int duration;
+}
