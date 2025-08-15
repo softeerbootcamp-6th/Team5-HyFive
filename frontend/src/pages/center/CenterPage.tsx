783374a056e83237db6fc3599d2fe36ade4e29f6
minjoengss <dotoriido@gmail.com>
2025-08-15 19:32:02 +0900

    feat: center 페이지에서 편집 페이지로 이동할 때 state로 폼 내부값 전달

--- frontend/src/pages/center/CenterPage.tsx
+++ frontend/src/pages/center/CenterPage.tsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router";
+import OriginalCarImg from "@/assets/images/OriginalCarImg.png";
+
 const CenterPage = () => {
-  return <>Center</>;
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("/center/edit", {
+      state: {
+        carImage: OriginalCarImg, // 이미지 경로를 state에 전달
+        carModel: "람보르기니",
+        carNumber: "12가1234",
+        maxPassenger: "8",
+        isLowFloor: true,
+      },
+    });
+  };
+
+  return (
+    <>
+      <button onClick={handleNavigate}>navigate to edit</button>
+    </>
+  );
 };
 
 export default CenterPage;
