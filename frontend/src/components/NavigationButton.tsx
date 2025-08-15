7a18c9f572fef5c8a1477a08403ab6ef19da845d
minjoengss <dotoriido@gmail.com>
2025-08-16 00:17:17 +0900

    feat: 페이지 이동 버튼 hover, press 애니메이션 구현

--- frontend/src/components/NavigationButton.tsx
+++ frontend/src/components/NavigationButton.tsx
@@ -1,6 +1,8 @@
 import { ChevronRightIcon } from "@/assets/icons";
+import usePressDetection from "@/hooks/usePressDetection";
 import { theme } from "@/styles/themes.style";
 import { css } from "@emotion/react";
+import { useState } from "react";
 import { Link } from "react-router";
 const { color, typography } = theme;
 
@@ -8,9 +10,13 @@ interface NavigationButtonProps {
   navigate: string;
 }
 const NavigationButton = ({ navigate }: NavigationButtonProps) => {
+  const [isPressing, setIsPressing] = useState(false);
+  const handlers = usePressDetection({
+    setIsPressing,
+  });
   return (
-    <Link to={navigate} css={ButtonContainer}>
-      <p css={ButtonText}>바로가기</p>
+    <Link to={navigate} css={ButtonContainer(isPressing)} {...handlers}>
+      <p>바로가기</p>
       <ChevronRightIcon />
     </Link>
   );
@@ -18,19 +24,39 @@ const NavigationButton = ({ navigate }: NavigationButtonProps) => {
 
 export default NavigationButton;
 
-const ButtonContainer = css`
+const ButtonContainer = (isPressing: boolean) => css`
   width: fit-content;
   display: flex;
-  flex-direction: row;
   align-items: center;
   gap: 12px;
   padding: 16px 24px;
   border-radius: 12px;
   border: 1px solid ${color.GrayScale.gray5};
-  cursor: pointer;
-`;
-
-const ButtonText = css`
   font: ${typography.Label.l1_semi};
-  color: ${color.GrayScale.gray5};
+  cursor: pointer;
+  transition:
+    background-color 0.25s ease,
+    color 0.25s ease;
+
+  background: ${isPressing ? color.GrayScale.black : color.GrayScale.white};
+  svg {
+    stroke: ${isPressing ? color.GrayScale.white : color.GrayScale.black};
+  }
+  &,
+  &:visited,
+  &:link {
+    color: ${isPressing ? color.GrayScale.white : color.GrayScale.gray5};
+  }
+
+  ${!isPressing &&
+  css`
+    &:hover {
+      background: ${color.GrayScale.gray5};
+      color: ${color.GrayScale.white};
+
+      svg {
+        stroke: ${color.GrayScale.white};
+      }
+    }
+  `}
 `;
