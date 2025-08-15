b70757240834075a6fdc42ef6ad996a85c63c486
didyou88 <caujisu2020@gmail.com>
2025-08-15 14:59:15 +0900

    refactor : 필터 모듈에서 사용하고 있는 dto 인퍼페이스 변경 DispatchLocation -> FilterDto

--- backend/src/main/java/hyfive/gachita/dispatch/UnDispatchService.java
+++ backend/src/main/java/hyfive/gachita/dispatch/UnDispatchService.java
@@ -19,24 +19,25 @@ public class UnDispatchService {
     private final BoundingBoxFilter boundingBoxFilter;
     private final HaversineFilter haversineFilter;
 
-    public List<DispatchLocation> createPath(double centerLat, double centerLng, double radiusMeters) {
-        // 1. 원본 데이터 가져오기
-        List<DispatchLocation> centerCandidates = centerDispatchLocationProvider.getAll();
-
-        // 2. Bounding Box 조건 생성
-        BoundingBoxCondition bboxCondition = BoundingBoxCondition.from(centerLat, centerLng, radiusMeters);
-
-        // 3. Bounding Box로 1차 필터링
-        List<DispatchLocation> bboxFiltered = boundingBoxFilter.filter(centerCandidates, bboxCondition);
-
-        // 4. Haversine(거리) 조건 생성
-        RadiusCondition radiusCondition = RadiusCondition.builder()
-                .centerLat(centerLat)
-                .centerLng(centerLng)
-                .radiusMeters(radiusMeters)
-                .build();
-
-        // 5. Haversine(거리)로 2차 필터링
-        return haversineFilter.filter(bboxFiltered, radiusCondition);
-    }
+    // TODO : DispatchLocation -> FilterDto 변경 후 수정
+//    public List<DispatchLocation> createPath(double centerLat, double centerLng, double radiusMeters) {
+//        // 1. 원본 데이터 가져오기
+//        List<DispatchLocation> centerCandidates = centerDispatchLocationProvider.getAll();
+//
+//        // 2. Bounding Box 조건 생성
+//        BoundingBoxCondition bboxCondition = BoundingBoxCondition.from(centerLat, centerLng, radiusMeters);
+//
+//        // 3. Bounding Box로 1차 필터링
+//        List<DispatchLocation> bboxFiltered = boundingBoxFilter.filter(centerCandidates, bboxCondition);
+//
+//        // 4. Haversine(거리) 조건 생성
+//        RadiusCondition radiusCondition = RadiusCondition.builder()
+//                .centerLat(centerLat)
+//                .centerLng(centerLng)
+//                .radiusMeters(radiusMeters)
+//                .build();
+//
+//        // 5. Haversine(거리)로 2차 필터링
+//        return haversineFilter.filter(bboxFiltered, radiusCondition);
+//    }
 }
