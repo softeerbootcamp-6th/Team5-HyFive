2007d586fee1a66071a3a036d0929977949b7254
minjoengss <dotoriido@gmail.com>
2025-08-15 12:31:45 +0900

    feat: 랜딩 페이지 호버시 각 영역 커지는 애니메이션 구현

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,63 @@
+import NavigationButton from "@/components/NavigationButton";
+import { theme } from "@/styles/themes.style";
+import { css } from "@emotion/react";
+const { color } = theme;
+
 const LandingPage = () => {
-  return <>Landing</>;
+  return (
+    <div css={LandingPageContainer}>
+      <div css={PartPageContainer} className="admin">
+        <div css={PartPageWrapper}>
+          <p css={PageTitleText}>관리자용 페이지</p>
+          <NavigationButton />
+        </div>
+      </div>
+      <div css={PartPageContainer} className="center">
+        <div css={PartPageWrapper}>
+          <p css={PageTitleText}>센터용 페이지</p>
+          <NavigationButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LandingPage;
+
+const LandingPageContainer = css`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+`;
+
+const PartPageContainer = css`
+  position: relative;
+  display: flex;
+  flex: 1;
+  transition: flex 0.5s ease;
+
+  &:hover {
+    flex: 2;
+  }
+  &.admin {
+    background-color: ${color.GrayScale.gray2};
+  }
+  &.center {
+    background-color: ${color.GrayScale.gray4};
+  }
+`;
+
+const PartPageWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  padding: 258px 100px;
+`;
+
+const PageTitleText = css`
+  font-size: 48px;
+  font-weight: 700;
+  letter-spacing: 1.92px;
+  color: ${color.GrayScale.gray6};
+`;
