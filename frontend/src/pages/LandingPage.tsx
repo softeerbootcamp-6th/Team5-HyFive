f6389c27fa8b1b909e4b0a1270bbaeb7a89d7abd
minjoengss <dotoriido@gmail.com>
2025-08-15 18:07:23 +0900

    feat: 로딩 페이지에 Lottie 할당 및 컴포넌트 영역 설정

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,21 +1,30 @@
+import HoverLottie from "@/components/HoverLottie";
 import NavigationButton from "@/components/NavigationButton";
 import { theme } from "@/styles/themes.style";
 import { css } from "@emotion/react";
 const { color } = theme;
+import LandingAdminLottie from "@/assets/lottie/landing-admin.json";
+import LandingCenterLottie from "@/assets/lottie/landing-center.json";
 
 const LandingPage = () => {
   return (
     <div css={LandingPageContainer}>
-      <div css={PartPageContainer} className="admin">
+      <div css={PartPageContainer}>
         <div css={PartPageWrapper}>
           <p css={PageTitleText}>관리자용 페이지</p>
           <NavigationButton navigate="admin" />
         </div>
+        <div css={LottieWrapper}>
+          <HoverLottie animationData={LandingAdminLottie} />
+        </div>
       </div>
-      <div css={PartPageContainer} className="center">
+      <div css={PartPageContainer}>
         <div css={PartPageWrapper}>
           <p css={PageTitleText}>센터용 페이지</p>
           <NavigationButton navigate="center" />
+        </div>
+        <div css={LottieWrapper}>
+          <HoverLottie animationData={LandingCenterLottie} />
         </div>
       </div>
     </div>
@@ -35,16 +44,11 @@ const PartPageContainer = css`
   position: relative;
   display: flex;
   flex: 1;
+  overflow: hidden;
   transition: flex 0.5s ease;
 
   &:hover {
     flex: 2;
-  }
-  &.admin {
-    background-color: ${color.GrayScale.gray2};
-  }
-  &.center {
-    background-color: ${color.GrayScale.gray4};
   }
 `;
 
@@ -52,7 +56,21 @@ const PartPageWrapper = css`
   display: flex;
   flex-direction: column;
   gap: 32px;
-  padding: 258px 100px;
+  padding: 258px 80px;
+  z-index: 1;
+`;
+
+const LottieWrapper = css`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+
+  & > svg {
+    object-position: left;
+  }
 `;
 
 const PageTitleText = css`
