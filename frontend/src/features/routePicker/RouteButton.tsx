a3c35f078d865277b10131f6aa06b8bfc57c1674
minjoengss <dotoriido@gmail.com>
2025-08-17 04:19:37 +0900

    feat: 개별 경로 버튼 구현

---
+++ frontend/src/features/routePicker/RouteButton.tsx
@@ -0,0 +1,59 @@
+import { css } from "@emotion/react";
+import { theme } from "@/styles/themes.style";
+import { CloseIcon, RouteIcon } from "@/assets/icons";
+import type { PassengerRoute } from "@/types/routeType.types";
+import type { MouseEvent } from "react";
+import PassengerRouteItem from "@/features/routePicker/PassengerRouteItem";
+const { color } = theme;
+
+interface RouteButtonProps {
+  selectedPassenger: PassengerRoute | null;
+  onOpen: () => void;
+  onReset: (e: MouseEvent) => void;
+}
+
+const RouteButton = ({
+  selectedPassenger,
+  onOpen,
+  onReset,
+}: RouteButtonProps) => {
+  return (
+    <div css={RouteButtonContainer} onClick={onOpen}>
+      <div css={TextButtonWrapper}>
+        {selectedPassenger ? (
+          <>
+            <PassengerRouteItem passenger={selectedPassenger} />
+            <CloseIcon onClick={onReset} />
+          </>
+        ) : (
+          <>
+            <RouteIcon fill={color.GrayScale.gray5} />
+            <p>탑승자 경로 보기</p>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RouteButton;
+
+const RouteButtonContainer = css`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  border-radius: 16px;
+  background: ${color.GrayScale.white};
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+`;
+
+const TextButtonWrapper = css`
+  display: flex;
+  flex-direction: row;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  padding: 6px 10px;
+`;
