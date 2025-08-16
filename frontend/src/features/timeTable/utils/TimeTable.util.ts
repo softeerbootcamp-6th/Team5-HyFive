e82f0d0ce7596052c5112b09b3d93d39aeaaa90b
jjamming <ohj3694@naver.com>
2025-08-16 22:54:48 +0900

    chore: 중복되는 유틸 함수 삭제

--- frontend/src/features/timeTable/utils/TimeTable.util.ts
+++ frontend/src/features/timeTable/utils/TimeTable.util.ts
@@ -53,20 +53,3 @@ export const isAllSlotsInSelectedWeek = (
 
   return slots.every((slot) => formattedWeek.includes(slot.rentalDate));
 };
-
-/**
- * 두 개의 시간 슬롯이 동일한지 확인합니다.
- * @param a 첫 번째 시간 슬롯
- * @param b 두 번째 시간 슬롯
- * @returns 동일하면 true, 아니면 false
- */
-export const isSameSlot = (
-  a: AvailableTimeSlotType,
-  b: AvailableTimeSlotType,
-) => {
-  return (
-    a.rentalDate === b.rentalDate &&
-    a.rentalStartTime === b.rentalStartTime &&
-    a.rentalEndTime === b.rentalEndTime
-  );
-};
