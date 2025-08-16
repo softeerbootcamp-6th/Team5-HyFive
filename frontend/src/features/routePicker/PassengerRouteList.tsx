3ad55329449664e769ecd9d8939fba0b9ccc0b2a
minjoengss <dotoriido@gmail.com>
2025-08-17 04:20:07 +0900

    feat: 개별 경로 정보 보여주는 기능 구현 및 영역 할당

---
+++ frontend/src/features/routePicker/PassengerRouteList.tsx
@@ -0,0 +1,76 @@
+import { css } from "@emotion/react";
+import { theme } from "@/styles/themes.style";
+import { CloseIcon } from "@/assets/icons";
+import type { PassengerRoute } from "@/types/routeType.types";
+import PassengerRouteItem from "@/features/routePicker/PassengerRouteItem";
+
+const { color } = theme;
+
+interface PassengerListProps {
+  passengers: PassengerRoute[];
+  onSelect: (passenger: PassengerRoute) => void;
+  onClose: () => void;
+}
+
+const PassengerRouteList = ({
+  passengers,
+  onSelect,
+  onClose,
+}: PassengerListProps) => {
+  return (
+    <div css={RouteDetailContainer}>
+      <ul css={PassengerWrapper}>
+        {passengers.map((passenger) => (
+          <li
+            key={passenger.id}
+            css={PartPassengerWrapper}
+            onClick={() => onSelect(passenger)}
+          >
+            <PassengerRouteItem passenger={passenger} />
+          </li>
+        ))}
+      </ul>
+      <div css={RouteButtonContainer} onClick={onClose}>
+        <CloseIcon />
+      </div>
+    </div>
+  );
+};
+
+export default PassengerRouteList;
+
+const RouteDetailContainer = css`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  gap: 12px;
+`;
+
+const PassengerWrapper = css`
+  display: flex;
+  padding: 12px;
+  flex-direction: column;
+  gap: 6px;
+  border-radius: 16px;
+  background: ${color.GrayScale.white};
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+`;
+
+const PartPassengerWrapper = css`
+  display: flex;
+  padding: 6px 10px;
+  align-items: center;
+  align-self: stretch;
+  gap: 8px;
+`;
+
+const RouteButtonContainer = css`
+  width: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 12px;
+  border-radius: 16px;
+  background: ${color.GrayScale.white};
+  box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+`;
