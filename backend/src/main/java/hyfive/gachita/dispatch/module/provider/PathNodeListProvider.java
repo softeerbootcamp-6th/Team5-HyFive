c030af049d618885f386c3b5c9fc4ffcde44754e
jin20fd <syj010820@naver.com>
2025-08-16 17:18:18 +0900

    chore: 경로별 노드 반환 provider 이름 변경

--- backend/src/main/java/hyfive/gachita/dispatch/module/provider/PathNodeListProvider.java
+++ backend/src/main/java/hyfive/gachita/dispatch/module/provider/PathNodeListProvider.java
@@ -1,10 +1,8 @@
 package hyfive.gachita.dispatch.module.provider;
 
-import hyfive.gachita.dispatch.dto.DispatchLocation;
-import hyfive.gachita.dispatch.dto.PathDispatchDto;
-import hyfive.gachita.dispatch.module.condition.Condition;
-import hyfive.gachita.dispatch.module.condition.PathCondition;
 import hyfive.gachita.application.path.respository.PathRepository;
+import hyfive.gachita.dispatch.dto.PathDispatchDto;
+import hyfive.gachita.dispatch.module.condition.PathCondition;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +10,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class NodeDispatchLocationProvider  {
+public class PathNodeListProvider  {
     private final PathRepository pathRepository;
 
     public List<PathDispatchDto> getByCondition(PathCondition condition) {
