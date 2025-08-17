aceb1047215ca7d497b14cef5185e7bdb23169c9
jin20fd <syj010820@naver.com>
2025-08-17 20:49:34 +0900

    feat: 센터 필터 실행 로직

--- backend/src/main/java/hyfive/gachita/dispatch/NewPathDispatchFlow.java
+++ backend/src/main/java/hyfive/gachita/dispatch/NewPathDispatchFlow.java
@@ -1,36 +1,30 @@
 package hyfive.gachita.dispatch;
 
+import hyfive.gachita.dispatch.dto.InitPathDto;
+import hyfive.gachita.dispatch.dto.NewBookDto;
 import hyfive.gachita.dispatch.module.filter.BoundingBoxFilter;
 import hyfive.gachita.dispatch.module.filter.HaversineFilter;
+import hyfive.gachita.dispatch.module.provider.CenterListProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class NewPathDispatchFlow {
 
+    private final CenterListProvider centerListProvider;
     private final BoundingBoxFilter boundingBoxFilter;
     private final HaversineFilter haversineFilter;
 
-    // TODO : DispatchLocation -> FilterDto 변경 후 수정
-//    public List<DispatchLocation> createPath(double centerLat, double centerLng, double radiusMeters) {
-//        // 1. 원본 데이터 가져오기
-//        List<DispatchLocation> centerCandidates = centerDispatchLocationProvider.getAll();
-//
-//        // 2. Bounding Box 조건 생성
-//        BoundingBoxCondition bboxCondition = BoundingBoxCondition.from(centerLat, centerLng, radiusMeters);
-//
-//        // 3. Bounding Box로 1차 필터링
-//        List<DispatchLocation> bboxFiltered = boundingBoxFilter.filter(centerCandidates, bboxCondition);
-//
-//        // 4. Haversine(거리) 조건 생성
-//        RadiusCondition radiusCondition = RadiusCondition.builder()
-//                .centerLat(centerLat)
-//                .centerLng(centerLng)
-//                .radiusMeters(radiusMeters)
-//                .build();
-//
-//        // 5. Haversine(거리)로 2차 필터링
-//        return haversineFilter.filter(bboxFiltered, radiusCondition);
-//    }
+    public void execute(NewBookDto newBookDto) {
+        List<InitPathDto> centerCandidates = centerListProvider.getAll();
+
+        // TODO : test 메서드 구현
+        List<InitPathDto> filteredCenterList = centerCandidates.stream()
+                .filter(center -> boundingBoxFilter.test(center, bboxCondition))
+                .filter(center -> haversineFilter.test(center, radiusCondition))
+                .toList();
+    }
 }
