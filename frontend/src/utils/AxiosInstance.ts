04e024189eec0074989e83fbd52fa466bedba875
minjoengss <dotoriido@gmail.com>
2025-08-18 20:24:12 +0900

    feat: AxiosInstance 생성 및 CustomError 부착

---
+++ frontend/src/utils/AxiosInstance.ts
@@ -0,0 +1,21 @@
+import { CustomError } from "@/utils/CustomError";
+import axios, { AxiosError } from "axios";
+
+const createInstance = () => {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_BASE_URL,
+    timeout: 5000,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  instance.interceptors.response.use(
+    (response) => response.data,
+    (error: AxiosError) => {
+      throw CustomError(error);
+    },
+  );
+};
+
+export const clientInstance = createInstance();
