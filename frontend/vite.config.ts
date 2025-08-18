fe4e8888fecc1705dc672979f1b5ac7301ee357b
minjoengss <dotoriido@gmail.com>
2025-08-18 16:42:24 +0900

    refactor: createHtmlPlugin 없이 env 존재만으로 동작하는 key 할당 방법 시도

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,31 +1,18 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { createHtmlPlugin } from "vite-plugin-html";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [
-      svgr(),
-      createHtmlPlugin({
-        inject: {
-          data: {
-            KAKAOMAP_KEY: env.VITE_KAKAOMAP_API_KEY,
-          },
-        },
-      }),
-      react(),
-    ],
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "src"),
-      },
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    test: {
-      globals: true,
-      environment: "jsdom",
-    },
-  };
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
 });
