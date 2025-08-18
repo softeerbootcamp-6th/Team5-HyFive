7b11e00931badafbbcd280d660d5df195f463508
minjoengss <dotoriido@gmail.com>
2025-08-18 15:45:06 +0900

    chore: index.html과 dev/production env 매칭

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,31 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { createHtmlPlugin } from "vite-plugin-html";
 import path from "path";
 import svgr from "vite-plugin-svgr";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [svgr(), react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [
+      svgr(),
+      createHtmlPlugin({
+        inject: {
+          data: {
+            KAKAOMAP_KEY: env.VITE_KAKAOMAP_API_KEY,
+          },
+        },
+      }),
+      react(),
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+    },
+  };
 });
