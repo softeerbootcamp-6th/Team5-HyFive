c2aeb75b8712bd083ff2efe4134ffa75f760049c
minjoengss <dotoriido@gmail.com>
2025-08-18 20:45:33 +0900

    chore: tanstack query devtools 라이브러리 설치 및 적용

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,17 @@ import Router from "@/routes";
 import "@/styles/global.style.css";
 import "@/styles/reset.style.css";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Router />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
