08cf520122074c10ad9867cef7cf1e5208420948
jin20fd <syj010820@naver.com>
2025-08-19 15:12:54 +0900

    refactor: path 모듈 분리

--- backend/src/main/java/hyfive/gachita/dispatch/NewPathDispatchFlow.java
+++ backend/src/main/java/hyfive/gachita/dispatch/NewPathDispatchFlow.java
@@ -8,6 +8,7 @@ import hyfive.gachita.dispatch.excepion.DispatchException;
 import hyfive.gachita.dispatch.module.condition.BoundingBoxCondition;
 import hyfive.gachita.dispatch.module.condition.RadiusCondition;
 import hyfive.gachita.dispatch.module.filter.BoundingBoxFilter;
+import hyfive.gachita.dispatch.module.filter.FinalNewPathValidator;
 import hyfive.gachita.dispatch.module.filter.HaversineFilter;
 import hyfive.gachita.dispatch.module.provider.CenterListProvider;
 import hyfive.gachita.dispatch.module.provider.IdleCarListProvider;
@@ -18,6 +19,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalTime;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 @RequiredArgsConstructor
@@ -27,6 +29,7 @@ public class NewPathDispatchFlow {
     private final RouteInfoProvider routeInfoProvider;
     private final BoundingBoxFilter boundingBoxFilter;
     private final HaversineFilter haversineFilter;
+    private final FinalNewPathValidator finalNewPathValidator;
 
     private final static int RADIUS_METERS = 500;
 
@@ -46,15 +49,8 @@ public class NewPathDispatchFlow {
         // duration, distance 정보
         // TODO: 모듈로 분리
         FinalNewPathDto bestPath = routeInfoProvider.getAll(filteredScheduleList, newBookDto).stream()
-                .filter(p -> p.totalDuration() < 3600)
-                // 센터 ~ 하차 시간이 모두 유휴시간에 포함되는지
-                .filter(p -> {
-                    LocalTime startTime = p.nodeList().get(0).time();
-                    LocalTime endTime = p.nodeList().get(p.nodeList().size() - 1).time();
-                    LocalTime rentalStartTime = p.path().rentalStartTime();
-                    LocalTime rentalEndTime = p.path().rentalEndTime();
-                    return rentalStartTime.isBefore(startTime) && endTime.isBefore(rentalEndTime);
-                })
+                .filter(finalNewPathValidator::isFirstPathDurationExceed)
+                .filter(finalNewPathValidator::isScheduleWithinRentalWindow)
                 .min(Comparator
                         // 차량 출발 시각과 유휴 시작 시간의 차이를 최소화
                         .comparing((FinalNewPathDto p) -> {
---
+++ backend/src/main/java/hyfive/gachita/dispatch/module/filter/FinalNewPathValidator.java
@@ -0,0 +1,23 @@
+package hyfive.gachita.dispatch.module.filter;
+
+import hyfive.gachita.dispatch.dto.FinalNewPathDto;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
+public class FinalNewPathValidator {
+    // 첫 운행의 최대 운행 시간 (1시간) 을 초과하는지 확인
+    public boolean isFirstPathDurationExceed(FinalNewPathDto path) {
+        return path.totalDuration() > 3600; // 1시간 = 3600초
+    }
+
+    // 실제 운행 스케줄(센터 출발 ~ 고객 하차)이 차량의 유휴 시간 내에 포함되는지 확인
+    public boolean isScheduleWithinRentalWindow(FinalNewPathDto path) {
+        LocalTime startTime = path.nodeList().get(0).time();
+        LocalTime endTime = path.nodeList().get(path.nodeList().size() - 1).time();
+        LocalTime rentalStartTime = path.path().rentalStartTime();
+        LocalTime rentalEndTime = path.path().rentalEndTime();
+        return rentalStartTime.isBefore(startTime) && endTime.isBefore(rentalEndTime);
+    }
+}
