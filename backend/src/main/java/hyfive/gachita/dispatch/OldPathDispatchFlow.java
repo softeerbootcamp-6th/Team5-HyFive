28fe7a072e4d0d3b20815d924592c41443328a06
didyou88 <caujisu2020@gmail.com>
2025-08-19 09:27:19 +0900

    feat : OldPathDispatchFlow에 전체 경로 중 bestPath 선출 로직 추가

--- backend/src/main/java/hyfive/gachita/dispatch/OldPathDispatchFlow.java
+++ backend/src/main/java/hyfive/gachita/dispatch/OldPathDispatchFlow.java
@@ -3,7 +3,9 @@ package hyfive.gachita.dispatch;
 import hyfive.gachita.client.kakao.RouteInfo;
 import hyfive.gachita.dispatch.dto.FinalPathCandidateDto;
 import hyfive.gachita.dispatch.dto.NewBookDto;
+import hyfive.gachita.dispatch.dto.NewPathDto;
 import hyfive.gachita.dispatch.dto.NodeDto;
+import hyfive.gachita.dispatch.excepion.DispatchExpectedException;
 import hyfive.gachita.dispatch.module.calculator.InsertPathInfoCalculator;
 import hyfive.gachita.dispatch.module.provider.SlotCandidateProvider;
 import lombok.RequiredArgsConstructor;
@@ -19,53 +21,70 @@ public class OldPathDispatchFlow {
     private final SlotCandidateProvider slotCandidateProvider;
     private final InsertPathInfoCalculator insertPathInfoCalculator;
 
+    private final List<FinalPathCandidateDto> finalPathCandidates = new  ArrayList<>();
+
     public void execute(List<Long> pathIds, NewBookDto newBook) {
         // TODO : 1. pathIds List<Long> 기준으로 path 테이블로 부터 아래 조건을 만족하는 path가 있는지 확인
 
         // TODO : 2. 위에 걸러진 path ID에 해당하는 Node getAll() NodeDispatchLocationDto
-        List<NodeDto> originalNodes = List.of();
+        List<List<NodeDto>> pathCandidates = List.of();
 
         // TODO : 3. 배차 차량이 존재하는가 확인 (완전 탐색 시작!!)
         // TODO : 3-1. 단일 경로 내 최적 경로 후보 선출
-        NodeDto newBookStartNode = NodeDto.newBookStartNodeFrom(newBook);
-        NodeDto newBookEndNode = NodeDto.newBookEndNodeFrom(newBook);
 
-        List<Integer> startSlotCandidates = slotCandidateProvider.findSlotCandidates(originalNodes, newBookStartNode);
-        List<Integer> endSlotCandidates = slotCandidateProvider.findSlotCandidates(originalNodes, newBookEndNode);
+        for(int i = 0; i < pathCandidates.size(); i++){
+            List<NodeDto> originalNodes = pathCandidates.get(i);
 
-        List<FinalPathCandidateDto> candidates = new ArrayList<>();
+            NodeDto newBookStartNode = NodeDto.newBookStartNodeFrom(newBook);
+            NodeDto newBookEndNode = NodeDto.newBookEndNodeFrom(newBook);
 
-        for (Integer si : startSlotCandidates) {
-            for (Integer ei : endSlotCandidates) {
-                if (si <= ei) {
-                    List<NodeDto> candidatePath = new ArrayList<>(originalNodes);
-                    candidatePath.add(ei, newBookEndNode);
-                    candidatePath.add(si, newBookStartNode);
+            List<Integer> startSlotCandidates = slotCandidateProvider.findSlotCandidates(originalNodes, newBookStartNode);
+            List<Integer> endSlotCandidates = slotCandidateProvider.findSlotCandidates(originalNodes, newBookEndNode);
 
-                    RouteInfo candidatePathRouteInfo = insertPathInfoCalculator.calculate(candidatePath);
-                    if (candidatePathRouteInfo != null) {
+            List<FinalPathCandidateDto> candidates = new ArrayList<>();
 
-                        FinalPathCandidateDto result = FinalPathCandidateDto.builder()
-                            .pathId(null) // TODO : 값 불러오기 필요
-                            .carId(null) // TODO : 값 불러오기 필요
-                            .newNodes(List.of(newBookStartNode, newBookEndNode))
-                            .oldNodes(new ArrayList<>(originalNodes))
-                            .totalDuration(candidatePathRouteInfo.totalDuration())
-                            .totalDistance(candidatePathRouteInfo.totalDistance())
-                            .build();
+            for (Integer si : startSlotCandidates) {
+                for (Integer ei : endSlotCandidates) {
+                    if (si <= ei) {
+                        List<NodeDto> candidatePath = new ArrayList<>(originalNodes);
+                        candidatePath.add(ei, newBookEndNode);
+                        candidatePath.add(si, newBookStartNode);
 
-                        candidates.add(result);
+                        RouteInfo candidatePathRouteInfo = insertPathInfoCalculator.calculate(candidatePath);
+                        if (candidatePathRouteInfo != null) {
+
+                            FinalPathCandidateDto result = FinalPathCandidateDto.builder()
+                                    .pathId(null) // TODO : 값 불러오기 필요
+                                    .carId(null) // TODO : 값 불러오기 필요
+                                    .newNodes(List.of(newBookStartNode, newBookEndNode))
+                                    .oldNodes(new ArrayList<>(originalNodes))
+                                    .totalDuration(candidatePathRouteInfo.totalDuration())
+                                    .totalDistance(candidatePathRouteInfo.totalDistance())
+                                    .build();
+
+                            candidates.add(result);
+                        }
                     }
                 }
             }
+
+            // 후보 들을 totalDuration이 작은 것이 앞에오는 순서대로, totalDuration이 같다면 totalDistance가 작은 것이 앞에 오는 순서대로 정렬.
+            candidates.stream()
+                    .min(Comparator.comparingInt(FinalPathCandidateDto::totalDuration)
+                            .thenComparingInt(FinalPathCandidateDto::totalDistance))
+                    .orElseThrow(() -> new DispatchExpectedException("단일 경로 내 총 이동시간"));
+
+            // 1순위 최종 후보에 올리기
+            finalPathCandidates.add(candidates.get(0));
         }
 
-        candidates.stream()
-            .min(Comparator.comparingInt(FinalPathCandidateDto::totalDuration)
-                    .thenComparingInt(FinalPathCandidateDto::totalDistance))
-            .orElse(null);
-
         // TODO : 3-2. 다중 경로 중 최적 경로 선출
+        FinalPathCandidateDto bestPath = finalPathCandidates.stream()
+                .min(Comparator
+                        .comparing(FinalPathCandidateDto::totalDuration)
+                        .thenComparing(FinalPathCandidateDto::totalDistance)
+                )
+                .orElseThrow(() -> new DispatchExpectedException("총 이동시간"));
 
         // TODO : 최종 경로 time 넣어주기!
     }
