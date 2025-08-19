5837d386c4db47cd2677a4b52e97ce45776a4a03
jjamming <ohj3694@naver.com>
2025-08-19 14:05:04 +0900

    feat: 버튼 컴포넌트 폰트 색상 props 추가

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 const { color, typography } = theme;
 
 type Color = "gray" | "orange";
+type textColor = "black" | "white";
 type Size = "small" | "big";
 interface ButtonProps {
   label: string;
@@ -12,6 +13,8 @@ interface ButtonProps {
   size?: Size;
   type?: "submit" | "button" | "reset";
   disabled?: boolean;
+  textColor?: textColor;
+  onClick?: () => void;
 }
 
 const Button = ({
@@ -20,6 +23,8 @@ const Button = ({
   size = "small",
   type = "button",
   disabled = false,
+  textColor = "white",
+  onClick = () => {},
 }: ButtonProps) => {
   const [isPressing, setIsPressing] = useState(false);
   const handlers = usePressDetection({
@@ -29,8 +34,9 @@ const Button = ({
     <button
       type={type}
       {...handlers}
-      css={ButtonContainer(bgColor, size, isPressing)}
+      css={ButtonContainer(bgColor, textColor, size, isPressing)}
       disabled={disabled}
+      onClick={() => onClick()}
     >
       {label}
     </button>
@@ -54,6 +60,7 @@ const BASE_COLOR_MAP = {
 
 const ButtonContainer = (
   bgColor: Color,
+  textColor: textColor,
   size: Size,
   isPressing: boolean,
 ) => css`
@@ -70,7 +77,9 @@ const ButtonContainer = (
   font: ${size === "small"
     ? typography.Label.l4_semi
     : typography.Body.b2_semi};
-  color: ${color.GrayScale.white};
+  color: ${textColor === "white"
+    ? color.GrayScale.white
+    : color.GrayScale.black};
   cursor: pointer;
   transition: all 0.2s ease;
 
