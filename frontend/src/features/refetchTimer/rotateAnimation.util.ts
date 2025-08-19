959285c9c00d9c3c1e5098f33e0402eefa849966
minjoengss <dotoriido@gmail.com>
2025-08-19 14:51:54 +0900

    feat: refetch 버튼 누를 때 아이콘 회전 애니메이션 할당

---
+++ frontend/src/features/refetchTimer/rotateAnimation.util.ts
@@ -0,0 +1,14 @@
+import { keyframes, css } from "@emotion/react";
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const rotating = css`
+  animation: ${rotate360} 1s linear;
+`;
