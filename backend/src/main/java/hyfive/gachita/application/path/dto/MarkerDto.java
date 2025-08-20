f8437862a4fd7157ccc21fed92c4e902e8354e12
didyou88 <caujisu2020@gmail.com>
2025-08-20 14:40:52 +0900

    feat : 경로 노드 정보 api dto 생성 - SegmentDto : 경로 세그먼트 - MarkerDto : 마커 정보 - HighlightDto : 특정 예약 경로 하이라이트 정보 - LatLng : Schema 추가

---
+++ backend/src/main/java/hyfive/gachita/application/path/dto/MarkerDto.java
@@ -0,0 +1,30 @@
+package hyfive.gachita.application.path.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import hyfive.gachita.application.node.NodeType;
+import hyfive.gachita.client.geocode.dto.LatLng;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.time.LocalTime;
+
+@Builder
+@Schema(description = "마커 dto")
+public record MarkerDto(
+        @Schema(description = "노드 ID", example = "1")
+        Long nodeId,
+
+        @Schema(description = "예약 ID", example = "1")
+        Long bookId,
+
+        @Schema(description = "노드 좌표 (lat, lng)")
+        LatLng point,
+
+        @Schema(description = "해당 노드를 지나는 예상 시간 (HH:mm)", example = "10:00")
+        @JsonFormat(pattern = "HH:mm")
+        LocalTime time,
+
+        @Schema(description = "노드 타입 (CENTER, START, END)", example = "START")
+        NodeType type
+) {
+}
---
+++ backend/src/main/java/hyfive/gachita/application/path/dto/SegmentDto.java
@@ -0,0 +1,18 @@
+package hyfive.gachita.application.path.dto;
+
+import hyfive.gachita.client.geocode.dto.LatLng;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Builder;
+
+import java.util.List;
+
+@Builder
+@Schema(description = "polyline을 그리기 위한 dto")
+public record SegmentDto(
+        @Schema(description = "세그먼트 ID", example = "1")
+        Long segmentId,
+
+        @Schema(description = "세그먼트에 포함된 좌표 리스트")
+        List<LatLng> pointList
+) {
+}
