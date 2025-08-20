89e476793259d18ab4294896197ef514bca420ce
jin20fd <syj010820@naver.com>
2025-08-20 18:34:09 +0900

    chore: 시간 형식 추가

--- backend/src/main/java/hyfive/gachita/application/path/dto/PathCursor.java
+++ backend/src/main/java/hyfive/gachita/application/path/dto/PathCursor.java
@@ -1,5 +1,6 @@
 package hyfive.gachita.application.path.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 
 import java.time.LocalTime;
@@ -7,6 +8,10 @@ import java.time.LocalTime;
 @Builder
 public record PathCursor(
         Long lastId,
+
+        @JsonFormat(pattern = "HH:mm")
         LocalTime lastStartTime,
+
+        @JsonFormat(pattern = "HH:mm")
         LocalTime lastEndTime
 ) {}
