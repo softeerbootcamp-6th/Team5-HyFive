1e3e40a80d00cf995bc0d85196566b84f9c27b85
jjamming <ohj3694@naver.com>
2025-08-23 01:05:58 +0900

    feat: 유휴시간 GET API 연동

---
+++ frontend/src/apis/TimeTableAPI.ts
@@ -0,0 +1,37 @@
+import { formatDateToYYMMDD } from "@/features/calender/Calender.util";
+import { clientInstance } from "@/utils/AxiosInstance";
+import { useQuery } from "@tanstack/react-query";
+
+interface TimeSlotAPIResponse {
+  isSuccess: boolean;
+  code: number;
+  message: string;
+  data: {
+    id: number;
+    carId: number;
+    rentalDate: string;
+    rentalStartTime: string;
+    rentalEndTime: string;
+  }[];
+}
+
+export const useGetTimeSlot = (selectedCarId: number, selectedWeek: Date[]) => {
+  const firstDayString = formatDateToYYMMDD(selectedWeek[0]);
+  const { data, isFetching, error } = useQuery<TimeSlotAPIResponse>({
+    queryKey: ["timeSlot", selectedCarId, firstDayString],
+    queryFn: () =>
+      clientInstance.get(
+        `/rental?car-id=${selectedCarId}&date=${firstDayString}`,
+      ),
+    staleTime: 5 * 60 * 1000,
+    gcTime: 30 * 60 * 1000,
+  });
+
+  const timeSlotData = data?.data;
+
+  return {
+    timeSlotData,
+    isFetching,
+    error,
+  };
+};
