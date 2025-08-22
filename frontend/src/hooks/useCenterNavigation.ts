0d2c82bd15261d0f1dfe01d1f4ac1a395c30271b
jjamming <ohj3694@naver.com>
2025-08-23 01:06:30 +0900

    feat: 차량 수정 시 ID가 아닌 차량의 데이터 객체를 넘기도록 변경

--- frontend/src/hooks/useCenterNavigation.ts
+++ frontend/src/hooks/useCenterNavigation.ts
@@ -1,4 +1,4 @@
-import { mockCarData } from "@/mocks/centerDetailMocks";
+import type { CarType } from "@/features/car/Car.type";
 import { useNavigate } from "react-router";
 
 export const useCarNavigation = () => {
@@ -8,19 +8,15 @@ export const useCarNavigation = () => {
     void navigate("/center/register");
   };
 
-  // TODO 재민 - API 연결되면, 차량 ID가 아닌 해당 차량의 데이터를 받아와야함
-  const navigateToEditCar = (selectedCarId: number) => {
-    const selectedCar = mockCarData.find((car) => car.carId === selectedCarId);
-    if (!selectedCar) return;
-
+  const navigateToEditCar = (carInfo: CarType) => {
     void navigate("/center/edit", {
       state: {
-        carId: selectedCar.carId,
-        carImage: selectedCar.carImgURL,
-        carModel: selectedCar.carName,
-        carNumber: selectedCar.carNum,
-        maxPassenger: String(selectedCar.capacity),
-        isLowFloor: selectedCar.isLowFloor,
+        carId: carInfo.carId,
+        carImage: carInfo.carImgURL,
+        carModel: carInfo.carName,
+        carNumber: carInfo.carNum,
+        maxPassenger: String(carInfo.capacity),
+        isLowFloor: carInfo.isLowFloor,
       },
     });
   };
