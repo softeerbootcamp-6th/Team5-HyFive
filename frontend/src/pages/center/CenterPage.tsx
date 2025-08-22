f9ea9ff8d539db7066144c5224570eb4925c958a
jjamming <ohj3694@naver.com>
2025-08-23 01:07:00 +0900

    feat: 센터 메인 페이지 API 연동

--- frontend/src/pages/center/CenterPage.tsx
+++ frontend/src/pages/center/CenterPage.tsx
@@ -7,8 +7,6 @@ import { theme } from "@/styles/themes.style";
 
 // 컴포넌트
 import CenterOverview from "@/features/centerOverview/CenterOverview";
-import CarInfoCard from "@/features/car/CarInfoCard";
-import CarAddCard from "@/features/car/CarAddCard";
 import { TimeTable } from "@/features/timeTable/components";
 import Calender from "@/features/calender/Calender";
 import ToolTip from "@/components/ToolTip";
@@ -18,7 +16,6 @@ import MonoButton from "@/components/MonoButton";
 import { WhiteEditIcon } from "@/assets/icons";
 
 // 데이터 & 유틸
-import { mockCarData, mockCenterData } from "@/mocks/centerDetailMocks";
 import {
   calenderReducer,
   initialState,
@@ -27,6 +24,11 @@ import { isFutureWeek } from "@/features/calender/Calender.util";
 import { useCarNavigation } from "@/hooks/useCenterNavigation";
 import Modal from "@/components/Modal";
 import { useCenterModal } from "@/hooks/useCenterModal";
+import { useGetCenterInfo } from "@/apis/CenterAPI";
+import { useGetCarList } from "@/apis/CarAPI";
+import { mapBackendCenterInfoToCenterOverview } from "@/features/centerOverview/CenterOverview.type";
+import { mapBackendCarListToCarList } from "@/features/car/Car.type";
+import CarList from "@/features/car/CarList";
 
 const { color, typography } = theme;
 const TOOLTIP_DATA = {
@@ -35,10 +37,23 @@ const TOOLTIP_DATA = {
 };
 
 const CenterPage = () => {
+  const {
+    centerInfoData,
+    isFetching: isCenterFetching,
+    error: CenterError,
+  } = useGetCenterInfo();
+  const {
+    carList,
+    isFetching: isCarFetching,
+    error: carError,
+  } = useGetCarList();
+
+  const mappedCenterData =
+    centerInfoData && mapBackendCenterInfoToCenterOverview(centerInfoData);
+  const mappedCarList = carList && mapBackendCarListToCarList(carList);
+
   // 상태
-  const [selectedCarId, setSelectedCarId] = useState<number>(
-    mockCarData[0]?.carId ?? null,
-  );
+  const [selectedCarId, setSelectedCarId] = useState<number>(0);
   const [state, dispatch] = useReducer(calenderReducer, initialState);
   const [isEditMode, setIsEditMode] = useState(false);
 
@@ -58,8 +73,33 @@ const CenterPage = () => {
     createModalContent,
   } = useCenterModal();
 
+  const handleClickEdit = (type: "edit" | "delete") => {
+    if (!selectedCarId || !mappedCarList) {
+      alert("선택된 차량이 없습니다. 차량 선택 후 진행해주세요.");
+      return;
+    }
+
+    const selectedCarInfo = mappedCarList.find(
+      (car) => car.carId === selectedCarId,
+    );
+
+    if (!selectedCarInfo) return;
+
+    if (type === "edit") {
+      openEditModal();
+    } else if (type === "delete") {
+      openDeleteModal();
+    }
+  };
+
   const handleEditConfirm = () => {
-    navigateToEditCar(selectedCarId);
+    const selectedCarInfo = mappedCarList!.find(
+      (car) => car.carId === selectedCarId,
+    );
+
+    if (!selectedCarInfo) return;
+
+    navigateToEditCar(selectedCarInfo);
     closeModal();
   };
 
@@ -88,34 +128,42 @@ const CenterPage = () => {
   return (
     <div css={PageContainer}>
       {/* 센터 정보 */}
-      <CenterOverview {...mockCenterData} />
+      <CenterOverview
+        {...mappedCenterData}
+        isLoading={isCenterFetching}
+        error={CenterError}
+      />
 
       {/* 등록된 차량 */}
       <div css={SectionWrapper}>
         <div css={CarSectionHeader}>
           <h4 css={SectionLabel}>등록된 차량</h4>
           <div css={ActionButtonGroup}>
-            <button css={CarEditTextStyle} onClick={openEditModal}>
+            <button
+              css={CarEditTextStyle}
+              onClick={() => handleClickEdit("edit")}
+            >
               수정하기
             </button>
             <div css={DividerStyle} />
-            <button css={CarEditTextStyle} onClick={openDeleteModal}>
+            <button
+              css={CarEditTextStyle}
+              onClick={() => handleClickEdit("delete")}
+            >
               삭제하기
             </button>
           </div>
         </div>
         <div css={ContentSection}>
-          {mockCarData.map((car) => (
-            <CarInfoCard
-              key={car.carId}
-              carData={car}
-              isSelected={selectedCarId === car.carId}
-              setIsSelected={setSelectedCarId}
-            />
-          ))}
-          {mockCarData.length < 6 && (
-            <CarAddCard onClick={() => navigateToRegisterCar()} />
-          )}
+          <CarList
+            canAddCar={true}
+            onAddCarClick={navigateToRegisterCar}
+            carList={mappedCarList}
+            selectedCarId={selectedCarId}
+            setSelectedCarId={setSelectedCarId}
+            isLoading={isCarFetching}
+            error={carError}
+          />
         </div>
       </div>
 
