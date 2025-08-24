c2ff8acb9473be9030dcb686e2cd43a1d3441c33
minjoengss <dotoriido@gmail.com>
2025-08-24 17:57:12 +0900

    feat: polling 전략으로 adaptive + exponential backoff 적용

--- frontend/src/apis/BookAPI.ts
+++ frontend/src/apis/BookAPI.ts
@@ -3,25 +3,51 @@ import { APIMatcher } from "@/utils/APIMatcher";
 import { clientInstance } from "@/utils/AxiosInstance";
 import TabMatcher from "@/utils/TabMatcher";
 import { useQuery } from "@tanstack/react-query";
+import { useRef } from "react";
 
 export const useGetBook = (activeTab: string) => {
+  // book status 서버용으로 변환
   const engBookStatus = TabMatcher.matchBookTypeKRToENG(activeTab);
   const bookStatus = TabMatcher.matchBookTypeClientToServer(engBookStatus);
-  const { data, isFetching, refetch } = useQuery<BookAPIResponse>({
+
+  // polling 주기 관리
+  const minInterval = 5000;
+  const maxInterval = 60000;
+  const intervalRef = useRef(minInterval);
+
+  const query = useQuery<BookAPIResponse>({
     queryKey: ["book", bookStatus],
     queryFn: () =>
       clientInstance.get(
         `/book/scroll?status=${bookStatus}&lastId=&lastCreatedAt=&size=100`,
       ),
     throwOnError: true,
-    refetchInterval: 600000,
+    refetchInterval: (query) => {
+      /**
+       * polling
+       * - 에러 발생시 interval 2배
+       * - 통신 성공+새 데이터 발생시 interval 최소값으로 설정
+       * - 통신 성공+새 데이터 없을시 interval 2배
+       */
+      if (query.state.error) {
+        intervalRef.current = Math.min(intervalRef.current * 2, maxInterval);
+      } else if (query.state.data && query.state.data?.data.items?.length > 0) {
+        intervalRef.current = minInterval;
+      } else {
+        intervalRef.current = Math.min(intervalRef.current * 2, maxInterval);
+      }
+      console.log(intervalRef.current);
+      return intervalRef.current;
+    },
   });
-  const bookData = data?.data.items.map((partData) =>
+
+  const bookData = query.data?.data.items.map((partData) =>
     APIMatcher.matchBookAPI(partData),
   );
+
   return {
     data: bookData,
-    isFetching,
-    refetch,
+    isFetching: query.isFetching,
+    refetch: query.refetch,
   };
 };
