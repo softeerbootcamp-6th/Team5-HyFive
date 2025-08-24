110acf58bb19b7abef0f9587976a68b005eda798
minjoengss <dotoriido@gmail.com>
2025-08-24 13:57:42 +0900

    feat: 개별 운행 경로 로직 useSuspenseQuery 도입 및 isError 제거

--- frontend/src/features/map/MapContent.tsx
+++ frontend/src/features/map/MapContent.tsx
@@ -1,18 +1,13 @@
 import { useGetNode } from "@/apis/ScheduleAPI";
 import MapCore from "@/features/map/MapCore";
-import { CustomError } from "@/utils/CustomError";
 
 const MapContent = ({ id }: { id: number }) => {
-  const { polyline, marker, highlight, isError } = useGetNode(id);
+  const { polyline, marker, highlight } = useGetNode(id);
 
+  // 데이터 없는 경우 대비
   const safeMarkerPath = marker ?? [];
   const safePolylinePath = polyline ?? [];
   const safeHighlightPath = highlight ?? [];
-
-  if (isError)
-    throw new CustomError({
-      message: "지도 관련 데이터가 존재하지 않습니다",
-    });
 
   return (
     <MapCore
