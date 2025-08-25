38c986f761a0a461dcb939b26bf26b5b10c51188
minjoengss <dotoriido@gmail.com>
2025-08-25 21:21:00 +0900

    feat: 실시간 운행 현황에서 데이터 갱신 테스트 코드 적용

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -3,6 +3,10 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      config.env.API_BASE = process.env.API_BASE || config.env.API_BASE;
+      return config;
+    },
   },
   viewportWidth: 1920,
   viewportHeight: 1200,
